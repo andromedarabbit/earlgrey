d5bed807c8a45f5ee6635ad5e9df0f97d198f60a
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-13 08:22:52 +0000

    복사 연산자를 막는 Uncopyable 클래스, LockfreeStack.h은 기완씨가 올린 LockfreeStack을 연습해볼 용도

---
+++ src/Earlgrey/inc/LockfreeStack.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace Earlgrey
+{
+	template<typename T>
+	struct LockfreeStack : private Uncopyable
+	{
+	public:
+		explicit LockfreeStack()
+		{
+		}
+
+		~LockfreeStack()
+		{
+
+		}
+
+		void Push(T newValue)
+		{
+		}
+
+
+	};
+}
---
+++ src/Earlgrey/inc/Uncopyable.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Earlgrey {
+
+	//! \ref Effective C++ 3th Edition Chapter 2
+	class Uncopyable
+	{
+	protected: // 파생된 객체에 대해서
+		Uncopyable() {}		// 생성과 소멸을
+		~Uncopyable() {}	// 허용합니다.
+
+	private:
+		Uncopyable(const Uncopyable&); // 하지만 복사는 방지합니다.
+		Uncopyable& operator = (const Uncopyable&);
+
+	};
+
+}
