a2b147b93cf3d2f5341e18bfbf65303d1761c3e4
joe.joey <joe.joey@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-17 11:03:27 +0000

    추가 작업 필요 일단..집에서 하려고 checkin 합니다.

---
+++ trunk/src/Earlgrey/inc/PAL_Windows.h
@@ -0,0 +1,62 @@
+#pragma  once
+
+// Windows Platform Adaptive Layer
+
+#include <winsock2.h>
+#include <windows.h>
+//#include <process.h>
+//#include <crtdbg.h>
+
+// Atomic Operator
+#define AtomicInc(a)									InterlockedIncrement((volatile long *)(&(a)))
+#define AtomicDec(a)									InterlockedDecrement((volatile long *)(&(a)))
+#ifdef _WIN64
+#define AtomicExch(Dest, Value)							InterlockedExchangePointer((volatile PVOID *)(&(Dest)), (PVOID)(Value))
+#else
+#define AtomicExch(Dest, Value)							InterlockedExchange((volatile long *)(&(Dest)), (long)(Value))
+#endif
+
+inline BOOL AtomicCAS(volatile LONG *Dest, LONG Compare, LONG Exchange)
+{
+	return InterlockedCompareExchange( Dest, Exchange, Compare ) == static_cast<LONG>(Compare);
+}
+
+inline BOOL AtomicCAS64(volatile LONGLONG *Dest, LONGLONG Compare, LONGLONG Exchange)
+{   
+#ifdef _WIN64
+	return InterlockedCompareExchange64((volatile LONGLONG*)Dest, Exchange, Compare) == static_cast<LONGLONG>(Compare);
+#else
+	BOOL Return = TRUE;
+	__asm
+	{       
+		push ecx
+			push ebx
+			push eax
+			push edx
+			push edi
+
+			mov edx, dword ptr [Compare] + 4
+			mov eax, dword ptr [Compare]
+		mov ecx, dword ptr [Exchange] + 4
+			mov ebx, dword ptr [Exchange]
+		mov edi, Dest
+			//    Compare (EDX:EAX) with operand, if equal (ECX:EBX) is stored
+			lock cmpxchg8b qword ptr [edi]
+		pop edi
+			pop edx
+			pop eax
+			pop ebx
+			pop ecx
+			je    success
+			mov dword ptr [Return], 0       
+success:
+	}
+	return Return;
+#endif
+}
+
+
+typedef unsigned char		UInt8;		// 8-bit  unsigned.
+typedef unsigned short		UInt16;		// 16-bit unsigned.
+typedef unsigned long		UInt32;		// 32-bit unsigned.
+typedef unsigned __int64	UInt64;		// 64-bit unsigned.
