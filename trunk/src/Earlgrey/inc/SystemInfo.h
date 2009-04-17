aef3ea4c46533db80d303df00921137cad8b76ac
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-17 04:02:12 +0000

    방금 뺴먹은 파일 II

---
+++ trunk/src/Earlgrey/inc/SystemInfo.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Uncopyable.h"
+
+namespace Earlgrey
+{
+	class SystemInfo : private Uncopyable
+	{
+	// private:
+		// explicit SystemInfo(); // 구현하지 않는다.
+
+	private:
+		SYSTEM_INFO m_SystemInfo;
+	public:
+		explicit SystemInfo()
+		{
+			GetSystemInfo(&m_SystemInfo);
+		}
+
+		// TODO: 아직 Assert가 없어서 대충 짜본다.
+		/*
+		void ValidateSystem() const
+		{			
+			WORD processorArchitecture = m_SystemInfo.wProcessorArchitecture;
+
+			BYTE processorRivision = HIBYTE(m_SystemInfo.wProcessorRevision );
+			if(processorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
+				return;
+
+			if(processorArchitecture == PROCESSOR_ARCHITECTURE_INTEL)
+			{
+				// 일단 Core Duo 2 이상만 정상처리하는 편이 좋지 않을까?
+				return;
+			}
+
+			// crash
+		}
+		*/
+
+		DWORD NumberOfProcessors() const
+		{
+			// TODO: GetLogicalProcessorInformation 가 아닌 GetSystemInfo 를 쓰는 게 맞나?
+			return m_SystemInfo.dwNumberOfProcessors;
+		}
+
+	};
+}
