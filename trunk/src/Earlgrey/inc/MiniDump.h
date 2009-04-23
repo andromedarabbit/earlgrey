e8508752bc75504ef624ee7e5989e77f1a4a5138
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-23 16:45:47 +0000

    집에서 작업하다 만 미니덤프. 여전히 가능성 점검 중이니 신경쓰지 마세요.

--- trunk/src/Earlgrey/inc/MiniDump.h
+++ trunk/src/Earlgrey/inc/MiniDump.h
@@ -1,35 +1,72 @@
-#pragma once
+#pragma once 
 #include "Uncopyable.h"
-#include "StlCustom.h"
+#include "LokiSingleton.h"
+#include "LokiThreads.h" // for Loki::SingleThreaded
+#include "NoLock.h"
+
+#ifndef _WINDOWS_
+#include <windows.h>
+#endif
+
+
+// #pragma comment(lib, "dbghelp")
 
 namespace Earlgrey
 {
-	////////////////////////////////////////////////////////////////////////////////
-	/// \file MiniDump.h
-	/// \author excel96
-	/// \date 2003.11.18
-	////////////////////////////////////////////////////////////////////////////////
-
-#pragma once
 	class MiniDump : private Uncopyable
 	{
+		friend struct Loki::CreateStatic<MiniDump>;
+
+	public:
+		/// 덤프할 데이터의 수준
+		enum DumpLevel
+		{
+			DUMP_LEVEL_LIGHT,  ///< MiniDumpNormal을 사용
+			DUMP_LEVEL_MEDIUM, ///< MiniDumpWithDataSegs를 사용
+			DUMP_LEVEL_HEAVY   ///< MiniDumpWithFullMemory를 사용
+		};
+
+
 	private:
-		
+		DumpLevel s_DumpLevel;          ///< 덤프 레벨.
+		static bool      s_AddTimeStamp;       ///< 날짜 기반 덤프 파일 이름 사용
+		static TCHAR     s_AppName[_MAX_PATH]; ///< 덤프 파일 이름
+		static TCHAR     s_CallStack[8192];    ///< 콜스택 문자열
+		static TCHAR     s_Modules[8192];      ///< 모듈 문자열
+		static LPCTSTR   s_DialogTemplate;     ///< 대화창 템플릿
+		static DLGPROC   s_DialogProc;         ///< 대화창 프로시져
+
 
 	public:
 		/// \brief 미니 덤프 기능을 초기화한다.
-		// void Initialize(DumpLevel dumpLevel)
+		static void Install(DumpLevel dumpLevel, bool addTimestamp=true, 
+			LPCTSTR dialogTemplate=NULL, DLGPROC dialogProc=NULL);
+
+		/// \brief 콜스택 문자열을 반환한다.
+		static LPCTSTR GetCallStack() { return s_CallStack; }
+
+		/// \brief 모듈 문자열을 반환한다.
+		static LPCTSTR GetModules() { return s_Modules; }
+
 
 	private:
-		/*
-		/// \brief 예외에 대한 정보를 받아서, 미니 덤프 파일을 생성한다. 		
-		INT CreateMiniDump(LPEXCEPTION_POINTERS ExceptionInfo, MINIDUMP_TYPE InOption = MiniDumpNormal);
-*/
+		/// \brief 예외에 대한 정보를 받아서, 미니 덤프 파일을 생성한다. 
+		static LONG WINAPI WriteDump(PEXCEPTION_POINTERS exPtrs);
+
 		/// \brief 생성 금지
-		explicit MiniDump() 
-		{
-		}
+		cMiniDump() {}
+
+		/// \brief 복사 생성 금지
+		cMiniDump(const cMiniDump&) {}
+
+		/// \brief 대입 연산 금지
+		cMiniDump& operator = (const cMiniDump&) { return *this; }
+
+
 	};
-
-
+	
+	typedef 
+		Loki::SingletonHolder<MiniDump, Loki::CreateStatic, Loki::DefaultLifetime,  Loki::SingleThreaded, NoLock> 
+		MiniDumpSingleton
+		;
 }
