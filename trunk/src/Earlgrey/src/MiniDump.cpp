2e71e4c17362a4389eb13182c4c3ddd08e3e698e
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-23 09:16:20 +0000

    미니덤프는 집에 있는 소스코드를 중심으로 짤 거라 전부 지운다. Stl 라이브러리의 데이터 구조는 나중에 새 메모리 할당자를 도입할 거라 미리 준비한다.

--- trunk/src/Earlgrey/src/MiniDump.cpp
+++ trunk/src/Earlgrey/src/MiniDump.cpp
@@ -1,15 +1,2 @@
 #include "stdafx.h"
 #include "MiniDump.h"
-#include "Uncopyable.h"
-
-namespace Earlgrey
-{
-	class MiniDump : private Uncopyable
-	{
-	private: 
-		explicit MiniDump();
-
-	public:
-
-	};
-}
