7dcce1ad38f7d45d9e41f1a1b742823e8b300516
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-27 09:42:20 +0000

    테스트 중.

--- trunk/src/Earlgrey/src/EarlgreyAssert.cpp
+++ trunk/src/Earlgrey/src/EarlgreyAssert.cpp
@@ -7,8 +7,12 @@
 namespace boost
 {
 	//! \note 임시 코드
+	//! \ref http://www.codeproject.com/KB/debug/DebugBreakAnyway.aspx
 	void assertion_failed(char const * expr, char const * function, char const * file, long line)
 	{
 		std::wcout << expr << function << file << line << std::endl;
+
+		::DebugBreak();
+		// *((volatile INT *)0x1) = 1;
 	}
 }
