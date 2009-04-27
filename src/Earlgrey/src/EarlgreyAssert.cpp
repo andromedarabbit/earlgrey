51b16e0c034eadfe8daf4b20ea6c8685767ec8fd
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-27 04:44:55 +0000

    임시 assert 구현

--- src/Earlgrey/src/EarlgreyAssert.cpp
+++ src/Earlgrey/src/EarlgreyAssert.cpp
@@ -1,2 +1,14 @@
 #include "stdafx.h"
 #include "EarlgreyAssert.h"
+
+//! \note 임시 코드
+#include <iostream>
+
+namespace boost
+{
+	//! \note 임시 코드
+	void assertion_failed(char const * expr, char const * function, char const * file, long line)
+	{
+		std::wcout << expr << function << file << line << std::endl;
+	}
+}
