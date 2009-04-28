b2be222d2bc6e2c858d37c3bd3bf9cbaff55531b
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-28 03:15:27 +0000

    미리 컴파일된 헤더를 두 개로 쪼갠다. 하나는 Earlgrey 프로젝트에서 쓸 것이고, 다른 하나는 외부 라이브러리에 공개할 헤더다.

--- trunk/src/Earlgrey/inc/Earlgrey.h
+++ trunk/src/Earlgrey/inc/Earlgrey.h
@@ -1,11 +1,5 @@
 #pragma  once
 
-#if defined(_WIN32) || defined(_WIN64)
-#include "PAL_Windows.h"
-#else
-#error "아직은 다른 OS 는 지원하지 않습니다."
-#endif
-
 #include "LockfreePointer.h"
 #include "Lockfree.h"
 
--- trunk/src/Earlgrey/inc/stdafx.h
+++ trunk/src/Earlgrey/inc/stdafx.h
@@ -15,4 +15,4 @@
 
 // TODO: reference additional headers your program requires here
 
-#include "Earlgrey.h"
+#include "EarlgreyPrivate.h"
