215d29e43f165eca2d78084791681b5cf2da922a
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-28 09:09:26 +0000

    빠진 파일

---
+++ src/Earlgrey/inc/Macros.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define EARLGREY_ENUM_TO_STRING(enumName) TEXT(#enumName)
---
+++ src/Earlgrey/src/AppType.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "AppType.h"
+#include "Macros.h"
+
+namespace Earlgrey
+{
+	//! \note 임시 코드
+	const TCHAR * AppType::Names[] = {
+		EARLGREY_ENUM_TO_STRING(E_APPTYPE_NONE)
+		, NULL
+	};
+}
