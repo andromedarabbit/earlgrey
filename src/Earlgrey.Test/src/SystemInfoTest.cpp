b48f0ef7814f93e37d38a01b94bf25ce62382767
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-29 09:44:25 +0000

    단위 테스트를 고쳤다.

--- src/Earlgrey.Test/src/SystemInfoTest.cpp
+++ src/Earlgrey.Test/src/SystemInfoTest.cpp
@@ -9,19 +9,22 @@ namespace Earlgrey
 	namespace Test
 	{	
 		//! \note 임시 조치
-		/*
 		TEST(SystemInfoTest, BaseDirectory)
 		{
 			SystemInfo info;
 			
 			// 예상값: "c:\workspace\earlgrey\src\Win32-Debug\bin"	
 			_txstring directory = info.BaseDirectory();
-			std::size_t found = directory.find(TEXT("bin"));
-	
+			
 			// 나온 값이 bin으로 끝나는지 확인한다.
-			ASSERT_NE(_txstring::npos, found);
-			ASSERT_EQ(directory.size(), found + 3);
+			// std::size_t found = directory.find(TEXT("bin"));
+			// ASSERT_NE(_txstring::npos, found);
+			// ASSERT_EQ(directory.size(), found + 3);
+
+			_txstring suffix = directory.substr(directory.length() - 3, 3);
+			const TCHAR * const expected = TEXT("bin");
+			ASSERT_STREQ(expected, suffix.c_str());
 		}
-		*/
+		
 	}
 }
