5bf8d8b54f23a7e7c2a9b5a96ca9b0c487fde4bc
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-04-30 13:06:12 +0000

    WinSock 초기화

--- src/Earlgrey/inc/Socket.h
+++ src/Earlgrey/inc/Socket.h
@@ -8,6 +8,10 @@ namespace Earlgrey
 		explicit Socket();
 		virtual ~Socket();
 
+		// \note 이 함수는 다른 클래스에 넣는 편이 나을까?
+		static BOOL InitializeSubSystem();
+		
+
 		virtual void OnConnected();
 		virtual void OnDisconnected();
 
