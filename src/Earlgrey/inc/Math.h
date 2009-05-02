7e53c5421f8f6974eef4c377cdeb080fa1554c8a
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-02 11:17:39 +0000

    공부할겸 컴파일 타임에 Log2를 계산하는 Log2Static 을 구현했다. 동일하게 컴파일 타임에 계산하는 pow도 추가.

--- src/Earlgrey/inc/Math.h
+++ src/Earlgrey/inc/Math.h
@@ -12,6 +12,8 @@ namespace Earlgrey
 		template<typename IntType>
 		inline IntType Log2(IntType n) // For double, long double, float
 		{
+			// 내장 함수를 사용하는 프로그램은 함수 호출의 오버헤드가 없기 때문에 빠르게 실행되지만
+			// 추가 코드를 만들기 때문에 프로그램이 커질 수 있습니다.
 			EARLGREY_ASSERT(n > 0);
 
 			unsigned long index;
@@ -26,5 +28,46 @@ namespace Earlgrey
 			const DoubleType TWO = 2;
 			return log(n) / log(TWO);
 		}
+
+
+		//! 컴파일 타임에 계산이 가능하고 제일 빠르다. 단 N은 상수만 허용된다.
+		template<unsigned N>
+		inline unsigned Log2Static()
+		{
+			return  1 + Log2Static<N / 2>( );
+		}
+
+		template<>
+		inline unsigned Log2Static<2>()
+		{
+			return 1UL;
+		}
+
+		template<>
+		inline unsigned Log2Static<1>()
+		{
+			return 0UL;
+		}
+
+		// x의 N 승
+		template< unsigned N >  
+		inline double PowStatic( double x )  
+		{
+			return PowStatic<N % 2>( x )  *  PowStatic<N / 2>( x * x );
+		}
+
+		template<>
+		inline double PowStatic<1U>( double x ) 
+		{ 
+			return x;  
+		}
+
+		template<>
+		inline double PowStatic<0U>( double x )  
+		{  
+			UNREFERENCED_PARAMETER(x);
+			return 1.;  
+		}
+
 	}
 }
