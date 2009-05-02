02df45ad024d184dae68c68bc53b7a4081bcbd2a
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-02 11:17:39 +0000

    공부할겸 컴파일 타임에 Log2를 계산하는 Log2Static 을 구현했다. 동일하게 컴파일 타임에 계산하는 pow도 추가.

--- trunk/src/Earlgrey.Test/src/MathTest.cpp
+++ trunk/src/Earlgrey.Test/src/MathTest.cpp
@@ -20,10 +20,13 @@ namespace Earlgrey
 			ASSERT_EQ(10, Earlgrey::Math::Log2(n3));
 
 			unsigned long n4 = std::numeric_limits<unsigned long>::max();
-			ASSERT_EQ(31, Earlgrey::Math::Log2(n4));			
+			ASSERT_EQ(31, Earlgrey::Math::Log2(n4));
+
+			unsigned long n5 = 3;
+			ASSERT_EQ(1, Earlgrey::Math::Log2(n5));
 		}
 
-		TEST(MathTest, Log2)
+		TEST(MathTest, Log2Ex)
 		{
 			double n1 = 64;
 			INT64 retValue1 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n1));
@@ -40,6 +43,29 @@ namespace Earlgrey
 			long double n4 = std::numeric_limits<unsigned long>::max();
 			INT64 retValue4 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n4));
 			ASSERT_EQ(31, retValue4);
+
+			long double n5 = 3;
+			INT64 retValue5 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n5));
+			ASSERT_EQ(1, retValue5);
+		}
+
+		TEST(MathTest, Log2Ex2)
+		{
+			const unsigned long n1 = 64;
+			ASSERT_EQ(6, Earlgrey::Math::Log2Static<n1>());
+
+			const unsigned long n2 = 1;
+			ASSERT_EQ(0, Earlgrey::Math::Log2Static<n2>());
+
+			const unsigned long n3 = 1025;
+			ASSERT_EQ(10, Earlgrey::Math::Log2Static<n3>());
+
+			const unsigned long n4 = ULONG_MAX;
+			ASSERT_EQ(n4, std::numeric_limits<unsigned long>::max());
+			ASSERT_EQ(31, Earlgrey::Math::Log2Static<n4>());			
+
+			const unsigned long n5 = 3;
+			ASSERT_EQ(1, Earlgrey::Math::Log2Static<n5>());
 		}
 	}
 }
