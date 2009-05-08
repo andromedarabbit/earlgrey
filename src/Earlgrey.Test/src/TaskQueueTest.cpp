60fab0f88d975b9bd3b2ca33475377ca0b0c7d51
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-08 01:25:52 +0000

    어이쿠 커밋 안 했네

---
+++ src/Earlgrey.Test/src/TaskQueueTest.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include "taskqueue.h"
+#include <iostream>
+
+using namespace Earlgrey::Algorithm::Lockfree;
+
+namespace Earlgrey
+{
+	namespace Test
+	{
+		int intval_for_taskq = 0;
+
+		DECLARE_TASK1(SampleTask, int);
+		DEFINE_TASK(SampleTask)(int val)
+		{
+			intval_for_taskq = val;
+		}
+
+		DECLARE_TASK0(NoParamTask);
+		DEFINE_TASK(NoParamTask)()
+		{
+			intval_for_taskq = 100;
+		}
+
+		class TestTaskQueueClass : public TaskQueueClassBase
+		{
+			DECLARE_QUEUEABLE_CLASS( TestTaskQueueClass );
+		public:
+			TestTaskQueueClass() : _test(0) {}
+			DECLARE_METHOD0(Set1);
+			DECLARE_METHOD0(Set10);
+			DECLARE_METHOD2(AddTwoValues, int, int);
+		public:
+			int _test;
+		};
+
+		void TestTaskQueueClass::RawSet1()
+		{
+			_test = 1;
+		}
+
+		void TestTaskQueueClass::RawSet10()
+		{
+			_test = 10;
+		}
+
+		void TestTaskQueueClass::RawAddTwoValues(int a, int b)
+		{
+			_test = a + b;
+		}
+
+		TEST(TaskQueueTest, Simple)
+		{
+			// 아래 테스트는 단일 스레드에서 테스트하는 코드이므로 깨지면 안된다.
+			SimpleTaskQueue taskq;
+			taskq.Post(new SampleTask( 10 ));
+			EXPECT_TRUE( intval_for_taskq == 10 );
+			taskq.Post(new NoParamTask());
+			EXPECT_TRUE( intval_for_taskq == 100 );
+
+			TestTaskQueueClass* myQueueableClass = new TestTaskQueueClass();
+			myQueueableClass->Set1();
+			EXPECT_TRUE( myQueueableClass->_test == 1 );
+			myQueueableClass->Set10();
+			EXPECT_TRUE( myQueueableClass->_test == 10 );
+			myQueueableClass->AddTwoValues( 10, 100 );
+			EXPECT_TRUE( myQueueableClass->_test == 110 );
+		}
+	}
+}
