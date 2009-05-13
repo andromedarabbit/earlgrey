868c933e91089fb81749afc415c06c7da7bb66ac
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-13 00:43:36 +0000

    TLS를 이용한 싱글톤을 구현했다. 싱글톤을 새로 구현하지 않고 Loki::SingletonHolder를 확장했다. 그리고 StackAllocator 에 버그가 있는 듯 하다. 일단 임시조치를 취했다.

---
+++ src/Earlgrey.Test/src/ThreadLocalTest.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "ThreadLocal.hpp"
+
+namespace Earlgrey
+{
+	namespace Test
+	{
+		//TEST(ThreadLocalTest, IntTest)
+		//{
+		//	const int integerValue = 5;
+
+		//	ThreadLocal<int> tlsInteger(integerValue);
+		//	int tlsValue1 = tlsInteger.GetValue();
+
+		//	ASSERT_EQ(integerValue, tlsValue1);
+
+		//	int* tlsValue2 = &tlsValue1;
+		//	// int* tlsValue2 = tlsInteger;;
+
+		//	ASSERT_EQ(integerValue, *tlsValue2);
+		//}
+
+		//TEST(ThreadLocalTest, IntPtrTest)
+		//{
+		//	int integerValue = 3;
+
+		//	ThreadLocal<int*> tlsInteger(&integerValue);
+		//	int* tlsValue1 = tlsInteger.GetValue();
+
+		//	ASSERT_EQ(integerValue, *tlsValue1);
+
+		//	int tlsValue2 = *tlsInteger;;
+		//	ASSERT_EQ(integerValue, tlsValue2);
+		//}
+	}
+}
