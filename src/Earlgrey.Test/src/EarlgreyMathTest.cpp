8d77e40bd83640bcf48e75933cad816763f0137d
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-15 16:45:13 +0000

    메모리 관리자를 개발하다가 Log2를 더 정확하게 만들 좋은 아이디어가 떠올라서... ㅎㅎ

--- src/Earlgrey.Test/src/EarlgreyMathTest.cpp
+++ src/Earlgrey.Test/src/EarlgreyMathTest.cpp
@@ -104,30 +104,30 @@ namespace Earlgrey
 			ASSERT_EQ(1, Earlgrey::Math::Log2(n5));
 		}
 
-		TEST(EarlgreyMathTest, Log2Ex)
+		TEST(EarlgreyMathTest, Log2)
 		{
 			double n1 = 64;
-			INT64 retValue1 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n1));
+			INT64 retValue1 = static_cast<INT64>(Earlgrey::Math::Log2(n1));
 			ASSERT_EQ(6, retValue1);
 
 			float n2 = 1;
-			INT64 retValue2 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n2));
+			INT64 retValue2 = static_cast<INT64>(Earlgrey::Math::Log2(n2));
 			ASSERT_EQ(0, retValue2);
 
 			long double n3 = 1025;
-			INT64 retValue3 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n3));
+			INT64 retValue3 = static_cast<INT64>(Earlgrey::Math::Log2(n3));
 			ASSERT_EQ(10, retValue3);
 
 			long double n4 = std::numeric_limits<unsigned long>::max EARLGREY_PREVENT_MACRO_SUBSTITUTION ();
-			INT64 retValue4 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n4));
+			INT64 retValue4 = static_cast<INT64>(Earlgrey::Math::Log2(n4));
 			ASSERT_EQ(31, retValue4);
 
 			long double n5 = 3;
-			INT64 retValue5 = static_cast<INT64>(Earlgrey::Math::Log2Ex(n5));
+			INT64 retValue5 = static_cast<INT64>(Earlgrey::Math::Log2(n5));
 			ASSERT_EQ(1, retValue5);
 		}
 
-		TEST(EarlgreyMathTest, Log2Ex2)
+		TEST(EarlgreyMathTest, Log22)
 		{
 			const unsigned long n1 = 64;
 			ASSERT_EQ(6, Earlgrey::Math::Log2Static<n1>());
