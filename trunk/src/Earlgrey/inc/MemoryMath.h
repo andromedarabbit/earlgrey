48e8d3643b55c734dec21e1fc87c1de4e4c82663
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-19 23:17:28 +0000

    집에서 커밋 안 한 코드가 있어서 임시조치한다.

--- trunk/src/Earlgrey/inc/MemoryMath.h
+++ trunk/src/Earlgrey/inc/MemoryMath.h
@@ -8,9 +8,9 @@ namespace Earlgrey
 	{
 		//! \todo 메모리 구현과 관련된 수학 연산. 나중에 별도의 파일로 옮긴다.
 		template<typename IntType>
-		inline IntType NewOffsetForMemoryAligment(IntType alignment, IntType offset)
+		inline IntType NewMemoryAligmentOffset(IntType alignment, IntType offset)
 		{
-			EARLGREY_ASSERT(offset > 0);
+			EARLGREY_ASSERT(offset >= 0);
 			EARLGREY_ASSERT(std::numeric_limits<IntType>::is_integer == true);
 			return alignment + ((offset - 1) & ~(alignment - 1));
 		}
--- trunk/src/Earlgrey/src/SinglyLinkedList.cpp
+++ trunk/src/Earlgrey/src/SinglyLinkedList.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "SinglyLinkedList.h"
+#include "SinglyList.h"
 
 namespace Earlgrey
 {
