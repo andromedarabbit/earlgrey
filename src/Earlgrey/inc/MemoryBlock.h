e3b6fa936d004f7b3b5398f040ff2747367a2579
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-20 08:04:37 +0000

    MemoryBlock과 그 리스트 SuperMemoryBlock 을 테스트하는 중입니다.

--- src/Earlgrey/inc/MemoryBlock.h
+++ src/Earlgrey/inc/MemoryBlock.h
@@ -38,16 +38,16 @@ namespace Earlgrey
 		typedef MemoryBlock::size_type size_type; 
 		
 		explicit SuperMemoryBlock(size_type chunkSize, size_type blockSize)
-			: m_BlockSize( Math::NewMemoryAligmentOffset<size_type>(EARLGREY_DEFAULT_ALLOCATION_ALIGNMENT, sizeof(MemoryBlock) + blockSize) )
+			: m_InternalBlockSize( Math::NewMemoryAligmentOffset<size_type>(EARLGREY_DEFAULT_ALLOCATION_ALIGNMENT, sizeof(MemoryBlock) + blockSize) )
 			, m_ChunkSize( Math::NewMemoryAligmentOffset<size_type>(EARLGREY_DEFAULT_PAGE_ALIGNMENT, chunkSize) )
 			// , m_NumberOfBlocks( m_ChunkSize / m_BlockSize )
 		{
 			EARLGREY_ASSERT(chunkSize >= blockSize);
 			EARLGREY_ASSERT(chunkSize > 0 && blockSize > 0);
-			EARLGREY_ASSERT( Math::IsPowerOf2(m_BlockSize) );
+			EARLGREY_ASSERT( Math::IsPowerOf2(blockSize) );
 
 			// 강제할 필요는 없지만, 되도록 매개변수를 던지기 전에 바이트 수를 생각해보면 좋겠다.
-			EARLGREY_ASSERT( m_ChunkSize % m_BlockSize == 0 ); 
+			EARLGREY_ASSERT( m_ChunkSize % blockSize == 0 ); 
 		}
 
 		inline size_type ChunkSize() const
@@ -57,12 +57,12 @@ namespace Earlgrey
 
 		inline size_type BlockSize() const
 		{
-			return m_BlockSize;
+			return m_InternalBlockSize - sizeof(MemoryBlock);
 		}
 
 		inline size_type NumberOfBlocks() const
 		{
-			return m_ChunkSize / m_BlockSize;
+			return m_ChunkSize / m_InternalBlockSize;
 		}
 
 		inline MemoryBlock * Data()
@@ -103,24 +103,24 @@ namespace Earlgrey
 
 			for (size_type i = 0; i < count; i++)
 			{
-				MemoryBlock * node = reinterpret_cast<MemoryBlock*>(newChunk + m_BlockSize * i);
+				MemoryBlock * node = reinterpret_cast<MemoryBlock*>(newChunk + m_InternalBlockSize * i);
 				EARLGREY_ASSERT(node != NULL);
 				
-				new( reinterpret_cast<void*>(node)) MemoryBlock(m_BlockSize); // 생성자 호출
+				new( reinterpret_cast<void*>(node)) MemoryBlock(m_InternalBlockSize); // 생성자 호출
 				CreateFreeNode(node);
 			}
 		}
 
 		inline void CreateFreeNode(MemoryBlock * node)
 		{
-			EARLGREY_ASSERT(node->m_BlockSize == m_BlockSize);
+			EARLGREY_ASSERT(node->m_BlockSize == m_InternalBlockSize);
 			InterlockedPushEntrySinglyList(&m_BlockHead, &node->m_Item);
 		}
 		
 
 	private:
 		size_type m_ChunkSize;
-		size_type m_BlockSize;
+		size_type m_InternalBlockSize;
 
 		SinglyListHead m_BlockHead;
 
