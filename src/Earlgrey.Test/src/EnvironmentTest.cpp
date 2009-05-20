6549b23ed0507fe5ff24da97fac787bd54dde01b
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-20 00:51:52 +0000

    생각난 김에 윈도우 계정의 이름, 컴퓨터의 NetBIOS 이름, 현재 디렉터리 등의 정보를 조작하는 기능을 추가한다.

--- src/Earlgrey.Test/src/EnvironmentTest.cpp
+++ src/Earlgrey.Test/src/EnvironmentTest.cpp
@@ -2,6 +2,7 @@
 #include "Environment.h"
 #include "StlCustom.h"
 #include "StringHelper.h"
+#include "Path.h"
 
 using namespace Earlgrey;
 
@@ -9,6 +10,15 @@ namespace Earlgrey
 {
 	namespace Test
 	{	
+		void ASSERT_ENDS_WITH(const _txstring& expected, const _txstring& value)
+		{
+			size_t suffixLength = expected.length();
+
+			_txstring suffix = value.substr(value.length() - suffixLength, suffixLength);
+
+			ASSERT_TRUE(StringHelper::EndsWith<STRCMP_CURRENT_CULTURE_IGNORECASE>(suffix, expected));
+		}
+
 		//! \note 임시 조치
 		TEST(EnvironmentTest, BaseDirectory)
 		{	
@@ -16,10 +26,58 @@ namespace Earlgrey
 			_txstring directory = Environment::BaseDirectory();
 			
 			// 나온 값이 bin으로 끝나는지 확인한다.
-			_txstring suffix = directory.substr(directory.length() - 3, 3);
-			const TCHAR * const expected = TEXT("bin");
-			ASSERT_TRUE(StringHelper::EndsWith<STRCMP_CURRENT_CULTURE_IGNORECASE>(suffix, expected));
+			_txstring expected = TEXT("bin");
+			ASSERT_ENDS_WITH(expected, directory);
+		}
+
+		TEST(EnvironmentTest, ProcessorCount)
+		{
+			DWORD count = Environment::ProcessorCount();
+			DWORD expected = 1;
+			ASSERT_GE(count, expected);
 		}
 		
+		TEST(EnvironmentTest, GetCurrentDirectory)
+		{
+			// 예상값: "c:\workspace\earlgrey\trunk\src\Earlgrey.Test"
+			_txstring directory = Environment::CurrentDirectory();
+			_txstring expected = TEXT("Earlgrey.Test");
+			ASSERT_ENDS_WITH(expected, directory);
+		}
+
+		TEST(EnvironmentTest, SetCurrentDirectory)
+		{
+			// 예상값: "c:\workspace\earlgrey\trunk\src\Earlgrey.Test"
+			_txstring oldDirectory = Environment::CurrentDirectory();
+			_txstring newDirectory = Path::Combine(oldDirectory, TEXT(".."));
+
+			ASSERT_TRUE(Environment::CurrentDirectory(newDirectory));
+			_txstring changedDirectory = Environment::CurrentDirectory();
+			_txstring expected = TEXT("src");
+			ASSERT_ENDS_WITH(expected, changedDirectory);
+
+			ASSERT_TRUE(Environment::CurrentDirectory(oldDirectory));
+			changedDirectory = Environment::CurrentDirectory();
+			expected = TEXT("Earlgrey.Test");
+			ASSERT_ENDS_WITH(expected, changedDirectory);
+		}
+
+		TEST(EnvironmentTest, SetCurrentDirectoryWithEmptyString)
+		{
+			ASSERT_THROW(Environment::CurrentDirectory(_TEXT("")), std::invalid_argument);
+		}
+
+		TEST(EnvironmentTest, MachineName)
+		{			
+			_txstring name = Environment::MachineName();
+			ASSERT_TRUE(name.length() > 0);
+		}
+
+
+		TEST(EnvironmentTest, UserDomainName)
+		{		
+			_txstring name = Environment::UserDomainName();
+			ASSERT_TRUE(name.length() > 0);
+		}
 	}
 }
