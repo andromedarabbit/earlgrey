7bad85fd87d639dde829fa9dd3903d1802436149
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-05-20 15:11:27 +0000

    기능상 변화는 없음.

--- src/Earlgrey/src/MemoryBlock.cpp
+++ src/Earlgrey/src/MemoryBlock.cpp
@@ -3,5 +3,20 @@
 
 namespace Earlgrey
 {
+	void SuperMemoryBlock::CreateFreeNodes()
+	{
+		BYTE * newChunk = static_cast<BYTE*>( AllocChunk() );
 
+		const size_type count = NumberOfBlocks();
+		EARLGREY_ASSERT(count > 0);
+
+		for (size_type i = 0; i < count; i++)
+		{
+			MemoryBlock * node = reinterpret_cast<MemoryBlock*>(newChunk + m_InternalBlockSize * i);
+			EARLGREY_ASSERT(node != NULL);
+
+			new( reinterpret_cast<void*>(node)) MemoryBlock(m_InternalBlockSize); // 생성자 호출
+			CreateFreeNode(node);
+		}
+	}
 }
