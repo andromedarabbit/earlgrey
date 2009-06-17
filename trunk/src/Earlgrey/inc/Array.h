b5c7abf81ea02ab77f9a1f1d614d6115b72da63b
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-06-17 14:56:08 +0000

    array 추가 예정. 일단 주석처리

---
+++ trunk/src/Earlgrey/inc/Array.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//namespace Earlgrey { namespace DataStructure
+//
+//	template<typename T, size_t Size>
+//	class Array
+//	{
+//	public:
+//		explicit Array() {}
+//		~Array() {}
+//
+//	private:
+//		T _data[Size];
+//	};
+//
+//}}
