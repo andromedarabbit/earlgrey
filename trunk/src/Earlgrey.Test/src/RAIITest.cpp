a4d5a000359efc28596def59a0deeded45b71cd0
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-09-08 14:10:34 +0000

    1. App(.exe) to Static library(.lib). 2. remove useless codes.

--- trunk/src/Earlgrey.Test/src/RAIITest.cpp
+++ trunk/src/Earlgrey.Test/src/RAIITest.cpp
@@ -29,38 +29,5 @@ namespace Earlgrey
 			}
 			ASSERT_EQ(256, ArrayItem::DELETE_COUNT);			
 		}
-
-		/*
-		struct LocalMemoryItem
-		{
-			static int LOCAL_MEMORY_DELETE_COUNT;
-
-			~LocalMemoryItem() 
-			{
-				LOCAL_MEMORY_DELETE_COUNT++;
-			}
-		};
-		int LocalMemoryItem::LOCAL_MEMORY_DELETE_COUNT = 0;
-
-		TEST(RAIITest, LocalMemoryDeleter)
-		{
-			LocalMemoryItem* item = NULL;
-			ASSERT_EQ(0, LocalMemoryItem::LOCAL_MEMORY_DELETE_COUNT);
-			{
-				HLOCAL handle = ::LocalAlloc(LPTR, sizeof(LocalMemoryItem));
-				ASSERT_TRUE(handle != NULL);
-				item = (LocalMemoryItem*)(::LocalLock(handle));
-				ASSERT_TRUE(item != NULL);
-				
-				ASSERT_FALSE(IsBadReadPtr(item, sizeof(LocalMemoryItem)));
-				// item->Item();
-
-				shared_ptr<HLOCAL> bufferManagement(handle, &LocalFree );
-								
-			}
-			ASSERT_TRUE(IsBadReadPtr(item, sizeof(LocalMemoryItem)));
-			ASSERT_EQ(1, LocalMemoryItem::LOCAL_MEMORY_DELETE_COUNT);		
-		}
-		*/
 	}
 }
