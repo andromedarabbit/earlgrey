f18ef35acfe12087d8c8ea3984e979eb8174a13e
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-09-21 20:24:06 +0000

    primitive type과 object type에 따라 다른 deletor가 사용되도록 수정

--- src/Earlgrey.Test/src/SharedPtrHelperTest.cpp
+++ src/Earlgrey.Test/src/SharedPtrHelperTest.cpp
@@ -4,15 +4,38 @@
 namespace Earlgrey
 {
 	namespace Test
-	{		
+	{
 		TEST(SharedPtrHelperTest, Basic)
 		{
+			class A
+			{
+			public:
+				A() : i(0) {}
+				~A() 
+				{
+					i = 100;
+				}
+				int i;
+			};
+
 			Earlgrey::StlGreedyAllocator<int> alloc;
 			std::tr1::shared_ptr<int> p = Earlgrey::create_shared_ptr<int>( alloc.allocate(1) );
 			ASSERT_NE( p.get(), static_cast<int*>(NULL) );
 
 			std::tr1::shared_ptr<int> p2 = p;
-			p2.reset( alloc.allocate(1), Earlgrey::StlCustomDeletor<int>(), Earlgrey::StlDefaultAllocator<int>::Type() );
+			p2.reset( alloc.allocate(1), Earlgrey::StlCustomDeletor<int>::Type(), Earlgrey::StlDefaultAllocator<int>::Type() );
+
+			Earlgrey::StlGreedyAllocator<A> alloc2;
+			A *a = alloc2.allocate(1);
+			a->A::A();
+			std::tr1::shared_ptr<A> pa = Earlgrey::create_shared_ptr<A>( a );
+			ASSERT_EQ( pa->i, 0 );
+
+			Earlgrey::StlGreedyAllocator<int*> alloc3;
+			int** ipp = alloc3.allocate(1);
+			*ipp = new int(3);
+			std::tr1::shared_ptr<int*> ppi = Earlgrey::create_shared_ptr<int*>( ipp );
+			delete *ppi;
 		}
 	}
 }
