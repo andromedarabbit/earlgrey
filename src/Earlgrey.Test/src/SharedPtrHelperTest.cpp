7f4b537fea1dbf546e08089d078e0fe003cd2cf1
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-09-22 01:00:04 +0000

    share_ptr helper 약간 개선

--- src/Earlgrey.Test/src/SharedPtrHelperTest.cpp
+++ src/Earlgrey.Test/src/SharedPtrHelperTest.cpp
@@ -19,22 +19,22 @@ namespace Earlgrey
 			};
 
 			Earlgrey::StlGreedyAllocator<int> alloc;
-			std::tr1::shared_ptr<int> p = Earlgrey::create_shared_ptr<int>( alloc.allocate(1) );
+			std::tr1::shared_ptr<int> p = Earlgrey::make_ptr<int>( alloc.allocate(1) );
 			ASSERT_NE( p.get(), static_cast<int*>(NULL) );
 
 			std::tr1::shared_ptr<int> p2 = p;
-			p2.reset( alloc.allocate(1), Earlgrey::StlCustomDeletor<int>::Type(), Earlgrey::StlDefaultAllocator<int>::Type() );
+			Earlgrey::reset_ptr( p2, alloc.allocate(1) );
 
 			Earlgrey::StlGreedyAllocator<A> alloc2;
 			A *a = alloc2.allocate(1);
 			a->A::A();
-			std::tr1::shared_ptr<A> pa = Earlgrey::create_shared_ptr<A>( a );
+			std::tr1::shared_ptr<A> pa = Earlgrey::make_ptr<A>( a );
 			ASSERT_EQ( pa->i, 0 );
 
 			Earlgrey::StlGreedyAllocator<int*> alloc3;
 			int** ipp = alloc3.allocate(1);
 			*ipp = new int(3);
-			std::tr1::shared_ptr<int*> ppi = Earlgrey::create_shared_ptr<int*>( ipp );
+			std::tr1::shared_ptr<int*> ppi = Earlgrey::make_ptr<int*>( ipp );
 			delete *ppi;
 		}
 	}
--- src/Earlgrey/inc/shared_ptr_helper.h
+++ src/Earlgrey/inc/shared_ptr_helper.h
@@ -6,52 +6,70 @@ namespace Earlgrey {
 
 	//! \todo memory manager 종류에 따라 deletor를 각각 만들어 줘야 함.
 
-	template<typename T>
-	struct StlPrimitiveTypeDeletor
+	struct GreedyDeleter
 	{
-		void operator()(T* p)
+		static void Delete(void* p)
 		{
-			StlDefaultAllocator<T>::Type _Alloc;
-			_Alloc.deallocate( p, sizeof(T) );
+			gGreedyAllocator::Instance().Free( p );
 		}
 	};
 
-	template<typename T>
-	struct StlObjectTypeDeletor
+	template<typename T, class Deleter>
+	struct StlPrimitiveTypeDeleter
+	{
+		void operator()(T* p)
+		{
+			Deleter::Delete( p );
+		}
+	};
+
+	template<typename T, class Deleter>
+	struct StlObjectTypeDeleter
 	{
 		void operator()(T* p)
 		{
 			p->~T();
-			StlDefaultAllocator<T>::Type _Alloc;
-			_Alloc.deallocate( p, sizeof(T) );
+			Deleter::Delete( p );
 		}
 	};
 
-	//! shared_ptr 에서 pointer의 deletor로 사용된다.
-	template<typename T>
-	struct StlCustomDeletor
+	//! shared_ptr 에서 pointer의 deleter로 사용된다.
+	template<typename T, class Deleter>
+	struct StlCustomDeleter
 	{
 		typedef typename mpl::if_<
 			std::numeric_limits<T>::is_specialized, 
-			StlPrimitiveTypeDeletor<T>, 
-			StlObjectTypeDeletor<T>
+			StlPrimitiveTypeDeleter<T,Deleter>, 
+			StlObjectTypeDeleter<T,Deleter>
 		>::type Type;
 	};
 
-	template<typename T>
-	struct StlCustomDeletor<T*>
+	template<typename T, class Deleter>
+	struct StlCustomDeleter<T*, Deleter>
 	{
-		typedef StlPrimitiveTypeDeletor<T*> Type;
+		typedef StlPrimitiveTypeDeleter<T*,Deleter> Type;
 	};
 
 
 
 	template<typename T>
-	std::tr1::shared_ptr<T> create_shared_ptr(T* p)
+	std::tr1::shared_ptr<T> make_ptr(T* p)
 	{
 		// 세번째 파라미터인 custom memory manager는 counter의 alloc/dealloc에 사용된다.
 		// counter alloc/dealloc에 사용되는 custom memory manager는 counter type으로 rebind되서 사용되므로
 		// template 인 T 가 어떤 타입이라도 상관 없다.
-		return std::tr1::shared_ptr<T>( p, StlCustomDeletor<T>::Type(), StlDefaultAllocator<T>::Type() );
+		return std::tr1::shared_ptr<T>( 
+			p, 
+			StlCustomDeleter<T, GreedyDeleter>::Type(), 
+			StlDefaultAllocator<T>::Type() );
+	}
+
+	template<typename T>
+	void reset_ptr(std::tr1::shared_ptr<T>& ptr, T* p)
+	{
+		ptr.reset( 
+			p, 
+			StlCustomDeleter<T, GreedyDeleter>::Type(), 
+			StlDefaultAllocator<T>::Type() );
 	}
 }
