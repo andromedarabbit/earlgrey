5382effa4dcbd27d9653b8985d4309b865fdd277
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-10-04 15:21:42 +0000

    Simple implementation of lexical_cast. This is much simpler than lexical_cast of boost library.

---
+++ trunk/src/Earlgrey.Test/src/LexicalCastTest.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "lexical_cast.hpp"
+
+#include "tstring.h"
+#include "txstring.h"
+
+namespace Earlgrey
+{
+	namespace Test
+	{
+		using namespace std;
+
+		TEST(StringConversionTest, ToString1)
+		{
+			const double sourceNumber = 12.345;
+			_txstring str = String::lexical_cast<_txstring>(sourceNumber);
+
+			_txstring expected = TEXT("12.345");
+			ASSERT_STREQ(expected.c_str(), str.c_str());
+		}
+
+		TEST(StringConversionTest, ToString2)
+		{
+			const double sourceNumber = 12.3456789;
+			_tstring str = String::lexical_cast<_tstring>(sourceNumber);
+
+			// Now has a precision limit 
+			// refer to http://gpgstudy.com/forum/viewtopic.php?topic=24400
+			_tstring expected = TEXT("12.345");
+			ASSERT_STRNE(TEXT("12.345"), str.c_str());
+		}
+	
+
+		TEST(StringConversionTest, FromString1)
+		{
+			double d = String::lexical_cast<double>("12.333");
+			ASSERT_EQ(12.333, d);
+		}
+	}
+}
