a2c092aef757e342d481af3d0799a40e40474b7c
initialjk <initialjk@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-10-25 17:12:08 +0000

    refs #17 * Branch로 올린다는게 trunk에 올려서.. rollback

--- src/Earlgrey/inc/StlGreedyAllocator.hpp
+++ src/Earlgrey/inc/StlGreedyAllocator.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "MemoryAllocator.h"
+#include "GreedyAllocator.h"
 #include <memory>
 
 namespace Earlgrey
@@ -58,14 +58,14 @@ namespace Earlgrey
 		void deallocate(pointer _Ptr, size_type)
 		{	// deallocate object at _Ptr, ignore size
 			// ::operator delete(_Ptr);
-			gMemoryAllocator::Instance().Free(_Ptr);
+			gGreedyAllocator::Instance().Free(_Ptr);
 		}
 
 		pointer allocate(size_type _Count)
 		{	// allocate array of _Count elements
 			// return (std::_Allocate(_Count, (pointer)0));
 			return static_cast<pointer>( 
-				gMemoryAllocator::Instance().Alloc(_Count * sizeof(_Ty))
+				gGreedyAllocator::Instance().Alloc(_Count * sizeof(_Ty))
 				);
 		}
 
