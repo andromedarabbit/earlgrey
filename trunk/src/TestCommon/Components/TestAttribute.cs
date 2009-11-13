e912dc8b80df1b3f64aed10af2d6b2cb448ff24a
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-11-13 04:48:14 +0000

    빠진것 추가; 이상하게 꼬여서 ^^;;

---
+++ trunk/src/TestCommon/Components/TestAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCommon.Components
+{
+    public enum TestType
+    {
+        AutoRun,        // void Test()
+        Interactive     // bool Test(string)
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class TestAttribute : Attribute
+    {
+        public TestAttribute(TestType testType)
+        {
+            this.TestType = testType;
+        }
+
+        public TestType TestType
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ trunk/src/TestCommon/Components/TestFixtureAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCommon.Components
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=false, Inherited=false)]
+    public class TestFixtureAttribute : Attribute
+    {
+        public TestFixtureAttribute(string prompt, string description)
+        {
+            Prompt = prompt;
+            Description = description;
+        }
+
+        public string Prompt
+        {
+            get;
+            private set;
+        }
+
+        public string Description
+        {
+            get;
+            private set;
+        }
+    }
+}
