9e9f9340c5db8d08a4a935b1142e2a5dc088baee
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-11-13 04:48:14 +0000

    빠진것 추가; 이상하게 꼬여서 ^^;;

---
+++ src/TestCommon/Components/SetUpAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCommon.Components
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class SetUpAttribute : Attribute
+    {
+    }
+}
