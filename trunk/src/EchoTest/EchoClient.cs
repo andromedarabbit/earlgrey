888fea982af11b9c3c0d1edd5954cd56ef7f7f9d
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2009-11-13 04:41:11 +0000

    echo test module 구현;

--- trunk/src/EchoTest/EchoClient.cs
+++ trunk/src/EchoTest/EchoClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using TestCommon.Net;
 using TestCommon;
 using TestCommon.Components;
@@ -11,5 +12,50 @@ namespace EchoText
     [TestFixture("ECHO > ", "Echo Test")]
     public class EchoClient
     {
+        TcpSocket socket = new TcpSocket();
+
+        [SetUp]
+        bool Initialize()
+        {
+            if (!socket.Connect(IPAddress.Loopback, 8000))
+            {
+                Reporter.Log(ReportType.Error, "Connection failed");
+                return false;
+            }
+
+            return true;
+        }
+
+        [TearDown]
+        void CleanUp()
+        {
+            socket.Close();
+        }
+
+        [Test(TestType.Interactive)]
+        bool Echo(string test)
+        {
+            if (test == "quit")
+            {
+                Reporter.Log(ReportType.Normal, "Quit!");
+                return false;
+            }
+
+            EchoProtocolPacket packet = new EchoProtocolPacket();
+            packet.Text = test;
+            OutputStream OStream = new OutputStream();
+            if (!packet.Write(OStream))
+            {
+                Reporter.Log(ReportType.Error, "Writing failed.");
+                return false;
+            }
+
+            if (!OStream.Flush(socket))
+            {
+                Reporter.Log(ReportType.Error, "Flushing failed.");
+                return false;
+            }
+            return true;
+        }
     }
 }
