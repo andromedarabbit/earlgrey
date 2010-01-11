390f122cd38aa02d46389c1460a981226d2eb79e
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-01-11 15:30:14 +0000

    깨진 테스트가 하나 더 늘었습니다. 주 스레드가 타이머를 지원하면 이 테스트가 통과될 겁니다.

--- trunk/src/Earlgrey.Test/src/TimerTest.cpp
+++ trunk/src/Earlgrey.Test/src/TimerTest.cpp
@@ -11,24 +11,26 @@ namespace Earlgrey
 			(*stateValue)++;
 		}
 
-		TEST(TimerTest, OneTimeTimer)
+		static void IncrementStateValueTest(StatePtr timerThreadIdState)
 		{
+			ThreadIdType * timerThreadIdPtr = reinterpret_cast<ThreadIdType*>(timerThreadIdState.get());
+
 			int * stateValue = new int(7); // 아래 StatePtr 이 스마트 포인터임
 			ASSERT_TRUE(stateValue != NULL);
 			StatePtr state(stateValue);
 
 			TimerCallback callback(&IncrementStateValue);
 
-			ASSERT_NE(IO_THREAD_ID_BEGIN, Thread::CurrentThread()->ThreadId());
+			ASSERT_NE(*timerThreadIdPtr, Thread::CurrentThread()->ThreadId());
 
 			Timer timer(
 				callback
 				, state
 				, TimeSpan::FromMilliseconds(1)
 				, TimeSpan::Zero // 단 한번만 실행
-				, IO_THREAD_ID_BEGIN
+				, *timerThreadIdPtr
 				);
-			
+
 			// TODO: 일단 귀찮아서 Sleep으로 했지만 Event를 쓰던가 해야 할 듯.
 			::Sleep(1000);
 
@@ -39,6 +41,51 @@ namespace Earlgrey
 			EXPECT_EQ(WAIT_OBJECT_0, ::WaitForSingleObject(waitHandle, 5000));
 
 			ASSERT_EQ(8, *stateValue);
+
+			(*timerThreadIdPtr) = INVALID_THREAD_ID;
+		}
+
+		TEST(TimerTest, OneTimeTimer)
+		{
+			ThreadIdType * stateValue = new ThreadIdType(IO_THREAD_ID_BEGIN); // 아래 StatePtr 이 스마트 포인터임
+			ASSERT_TRUE(stateValue != NULL);
+			StatePtr state(stateValue);
+
+			IncrementStateValueTest(state);
+
+			ASSERT_EQ(INVALID_THREAD_ID, *stateValue);
+		}
+
+		//! \todo 여전히 중복코드가 많다.
+		TEST(TimerTest, TwoTimers)
+		{
+			ThreadIdType * stateValue = new ThreadIdType(WIN_MAIN_THREAD_ID); // 아래 StatePtr 이 스마트 포인터임
+			ASSERT_TRUE(stateValue != NULL);
+			StatePtr state(stateValue);
+
+			TimerCallback callback(&IncrementStateValueTest);
+
+			ThreadIdType timerThreadID = IO_THREAD_ID_BEGIN;
+			ASSERT_NE(timerThreadID, Thread::CurrentThread()->ThreadId());
+
+			Timer timer(
+				callback
+				, state
+				, TimeSpan::FromMilliseconds(1)
+				, TimeSpan::Zero // 단 한번만 실행
+				, timerThreadID
+				);
+
+			// TODO: 일단 귀찮아서 Sleep으로 했지만 Event를 쓰던가 해야 할 듯.
+			::Sleep(1000);
+
+			HANDLE waitHandle = ::CreateEvent(NULL, TRUE, FALSE, NULL);
+			ASSERT_TRUE(waitHandle != NULL);
+			timer.Close(waitHandle);
+
+			EXPECT_EQ(WAIT_OBJECT_0, ::WaitForSingleObject(waitHandle, 5000));
+
+			ASSERT_EQ(INVALID_THREAD_ID, *stateValue);
 		}
 	}
 }
