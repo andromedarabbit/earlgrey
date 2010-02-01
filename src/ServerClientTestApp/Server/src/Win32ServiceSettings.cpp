06104a9fbf5ed5f54e8178cbad938b840220b7c6
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-02-01 13:03:33 +0000

    1. Earlgrey.Database 를 솔루션에서 뺌 2. Win32 Service 모드로 실행할 땐 중복 실행을 허용하지 않음

--- src/ServerClientTestApp/Server/src/Win32ServiceSettings.cpp
+++ src/ServerClientTestApp/Server/src/Win32ServiceSettings.cpp
@@ -1,10 +1,15 @@
 #include "stdafx.h"
 #include "Win32ServiceSettings.h"
 
+#include "Win32ServiceHelper.h"
+
 using namespace Earlgrey;
 
 BOOL Win32ServiceSettings::AllowOnlyOneInstance() const
 {
+	if(Win32ServiceHelper::RunByWin32Service())
+		return TRUE;
+
 	return FALSE;
 }
 
