bcfa99d57998acf51b8850271b4aed7694d03b4c
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-02-10 16:18:15 +0000

    BinaryReader, BinaryWriter에 memcpy 대신 memcpy_s 를 적용함.

--- src/Earlgrey.Test/src/BinaryReaderTest.cpp
+++ src/Earlgrey.Test/src/BinaryReaderTest.cpp
@@ -147,7 +147,7 @@ namespace Earlgrey
 
 			BinaryReader<BUFFER_T> reader(buf);
 			TCHAR retValue[128];
-			ASSERT_TRUE(reader.ReadString(retValue, _countof(tempStr)));
+			ASSERT_TRUE(reader.ReadString(retValue, _countof(retValue), _countof(tempStr)));
 			ASSERT_TRUE( _txstring(retValue) == ABCD );
 		}
 
@@ -163,7 +163,7 @@ namespace Earlgrey
 			
 			BinaryReader<BUFFER_T> reader(buf);
 			TCHAR retValue[128];
-			ASSERT_TRUE(reader.ReadString(retValue, _countof(tempStr)));
+			ASSERT_TRUE(reader.ReadString(retValue, _countof(retValue), _countof(tempStr)));
 			ASSERT_TRUE( _txstring(retValue) == ABCD );
 		}
 
@@ -185,7 +185,7 @@ namespace Earlgrey
 
 			BinaryReader<BUFFER_T> reader(buf);
 			BYTE retValue[128];
-			ASSERT_TRUE(reader.ReadBytes(retValue, _countof(ABCD)));
+			ASSERT_TRUE(reader.ReadBytes(retValue, _countof(retValue), _countof(ABCD)));
 			
 			for(int i=0; i<_countof(ABCD); i++)
 			{
