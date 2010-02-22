6628cc7aecbd339594483b59b82eb7af4555faac
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-02-22 04:56:30 +0000

    include counter 추가

---
+++ src/IncludeCounter/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace IncludeCounter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ResultView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.selectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.diagnosis = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.ResultView);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 467);
+            this.panel1.TabIndex = 0;
+            // 
+            // ResultView
+            // 
+            this.ResultView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.ResultView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ResultView.FullRowSelect = true;
+            this.ResultView.Location = new System.Drawing.Point(0, 0);
+            this.ResultView.Name = "ResultView";
+            this.ResultView.Size = new System.Drawing.Size(577, 467);
+            this.ResultView.TabIndex = 0;
+            this.ResultView.UseCompatibleStateImageBehavior = false;
+            this.ResultView.View = System.Windows.Forms.View.Details;
+            this.ResultView.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.ResultView_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Filename";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Count";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Recursive";
+            this.columnHeader3.Width = 80;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.diagnosis);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 469);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(577, 33);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // diagnosis
+            // 
+            this.diagnosis.Location = new System.Drawing.Point(3, 3);
+            this.diagnosis.Name = "diagnosis";
+            this.diagnosis.Size = new System.Drawing.Size(101, 23);
+            this.diagnosis.TabIndex = 0;
+            this.diagnosis.Text = "Diagnosis";
+            this.diagnosis.UseVisualStyleBackColor = true;
+            this.diagnosis.Click += new System.EventHandler(this.diagnosis_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 502);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainForm";
+            this.Text = "Include Counter";
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView ResultView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.FolderBrowserDialog selectFolder;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button diagnosis;
+    }
+}
+
