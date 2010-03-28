0642b78cf71c5a53472839749d8bb40deaeafb60
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-03-28 17:35:12 +0000

    Socket.Bind 새로 구현 중.

--- trunk/src/Earlgrey.Test/src/SocketTest.cpp
+++ trunk/src/Earlgrey.Test/src/SocketTest.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Socket.h"
 
+#include "IPAddress.h"
 #include "SocketTest.h"
 
 namespace Earlgrey
@@ -20,6 +21,17 @@ namespace Earlgrey
 
 			const USHORT port = 9879; // 설마 선점한 녀석은 없겠지?
 			ASSERT_TRUE(socket.Bind(port));
+		}
+
+		TEST_F(SocketTest, BindNewVersion)
+		{
+			Socket socket;
+			// ASSERT_TRUE(socket.CreateTcpSocket());
+
+			const USHORT port = 9879; // 설마 선점한 녀석은 없겠지?
+			IPEndPoint localEp(IPAddress2::Any(), port);
+
+			ASSERT_TRUE(socket.Bind(localEp));
 		}
 
 
