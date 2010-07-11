5dd35465b8cd935826bf0440383b3f870d12f83d
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-07-11 16:12:21 +0000

    stream 객체를 좀더 세밀하게 구분하기(tsstream.h)

--- trunk/src/Earlgrey/inc/tsstream.h
+++ trunk/src/Earlgrey/inc/tsstream.h
@@ -10,20 +10,28 @@ namespace Earlgrey
 	typedef	std::stringstream _tstringstream;
 #endif
 
-#ifdef _UNICODE
 	typedef std::basic_stringstream<wchar_t, std::char_traits<wchar_t>,
-		StlDefaultAllocator<wchar_t>::Type > _txstringstream; // TODO: 이름을 x_tstringstream으로
-#else
+		StlDefaultAllocator<wchar_t>::Type > x_wstringstream;
+
 	typedef std::basic_stringstream<char, std::char_traits<char>,
-		StlDefaultAllocator<char>::Type > _txstringstream; // TODO: 이름을 x_tstringstream으로
-#endif
+		StlDefaultAllocator<char>::Type > x_stringstream; // TODO: 이름을 x_tstringstream으로
 
 #ifdef _UNICODE
-	typedef std::basic_stringstream<wchar_t, std::char_traits<wchar_t>,
-		StlStackAllocator<wchar_t> > auto_tstringstream;
+	typedef x_wstringstream _txstringstream; // TODO: 이름을 x_tstringstream으로
 #else
+	typedef x_stringstream _txstringstream; // TODO: 이름을 x_tstringstream으로
+#endif
+
+	typedef std::basic_stringstream<wchar_t, std::char_traits<wchar_t>,
+		StlStackAllocator<wchar_t> > auto_wstringstream;
+
 	typedef std::basic_stringstream<char, std::char_traits<char>,
-		StlStackAllocator<char> > auto_tstringstream;
+		StlStackAllocator<char> > auto_stringstream;
+
+#ifdef _UNICODE
+	typedef auto_wstringstream auto_tstringstream;
+#else
+	typedef auto_4545rt6y56stringstream auto_tstringstream;
 #endif
 
 }
