81308e52d3b3cfb1de21334d6024816d9be09573
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-08-08 04:23:48 +0000

    MSBuild 확장기능 RoboCopy. MSBuild Extension Pack 에 있던 기능인데 비트 연산에 따른 결과를 고려하지 않고 종료상태 값을 처리하는 문제가 있습니다.

---
+++ trunk/src/BuildTools/MSBuild.Earlgrey.Tasks.Tests/TaskUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSBuild.Earlgrey.Tasks.Tests
+{
+    internal class TaskUtility
+    {
+        public static string  BaseDirectory
+        {
+            get { return AppDomain.CurrentDomain.BaseDirectory; }
+        }
+    }
+}
