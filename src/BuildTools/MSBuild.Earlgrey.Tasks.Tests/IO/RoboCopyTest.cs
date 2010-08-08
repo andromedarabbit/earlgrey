81e10a4ff1d535ddd6c758332731af162da81fd3
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-08-08 04:23:48 +0000

    MSBuild 확장기능 RoboCopy. MSBuild Extension Pack 에 있던 기능인데 비트 연산에 따른 결과를 고려하지 않고 종료상태 값을 처리하는 문제가 있습니다.

---
+++ src/BuildTools/MSBuild.Earlgrey.Tasks.Tests/IO/RoboCopyTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using MSBuild.Earlgrey.Tasks.IO;
+using NUnit.Framework;
+
+namespace MSBuild.Earlgrey.Tasks.Tests.IO
+{
+    [TestFixtureAttribute]
+    public class RoboCopyTest
+    {
+        private static readonly string SrcDir;
+        private static readonly string DstDir;
+
+        static RoboCopyTest()
+        {
+            SrcDir = Path.Combine(
+                TaskUtility.BaseDirectory
+                , @"IO\RoboCopySrcFolder1"
+                );
+            DstDir = Path.Combine(
+                TaskUtility.BaseDirectory
+                , @"IO\RoboCopyDstFolder1"
+                );
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            if (Directory.Exists(DstDir))
+                Directory.Delete(DstDir, true);
+        }
+
+        [Test]
+        public void CopyFileWithParentDirectory()
+        {
+            RoboCopy roboCopy = new RoboCopy();
+            roboCopy.BuildEngine = new MockBuildEngine();
+
+            Assert.IsTrue(Directory.Exists(SrcDir));
+            Assert.IsFalse(Directory.Exists(DstDir));
+
+            roboCopy.SourceDir = new TaskItem(SrcDir);
+            roboCopy.DestinationDir = new TaskItem(DstDir);
+            roboCopy.Files = new ITaskItem[]
+                                 {
+                                     new TaskItem("*.*")
+                                 };
+            Assert.IsTrue(roboCopy.Execute());
+            Assert.IsTrue(Directory.Exists(DstDir));
+        }
+    }
+}
