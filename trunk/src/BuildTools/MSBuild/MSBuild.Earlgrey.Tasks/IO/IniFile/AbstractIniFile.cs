013d131af0cefc303d03bcc8163998e27c682de7
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-08-21 13:10:35 +0000

    MSBuild용 INI 파일 조작 기능 구현 중 1. 구글 코드에 있는 ini-parser (MIT 라이선스) 채택 섹션 이름이 없는 전역 섹션을 처리 못하는 문제가 있어서 소스코드를 수정함. 2. 섹션 목록 가져오는 기능 구현 3. 특정 키 값을 입력하는 기능 구현.
    공유폴더 기능 개선
    1. 빌드 스크립트에서 권한을 입력하지 못하던 문제를 해결함 
    (입력 값의 형식을 string 으로 해야 함)

---
+++ trunk/src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks/IO/IniFile/AbstractIniFile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IniParser;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Earlgrey.Tasks.IO.IniFile
+{
+    public abstract class AbstractIniFile : AbstractTask
+    {
+        private readonly FileIniDataParser _parser;
+        private IniData _parsedData;
+
+        protected AbstractIniFile()
+        {
+            _parser = new FileIniDataParser();
+        }
+
+        protected override bool ExecuteCommand()
+        {
+            _parsedData = _parser.LoadFile(FullPath);
+         
+            return true;
+        }
+
+        protected override bool ValidateParameters()
+        {
+            if(File.Exists(FullPath) == false)
+            {
+                Log.LogError( 
+                    string.Format("Ini file not found at '{0}'", FilePath)
+                    );
+                return false;
+            }
+
+            return true;
+        }
+
+        protected FileIniDataParser Parser
+        {
+            get
+            {
+                Debug.Assert(_parser != null);
+                return _parser;
+            }
+        }
+
+        protected IniData Data
+        {
+            get
+            {
+                Debug.Assert(_parsedData != null); 
+                return _parsedData;
+            }
+        }
+
+        [Required]
+        public ITaskItem FilePath { get; set; }
+
+        protected string FullPath
+        {
+            get { return FilePath.GetMetadata("FullPath"); }
+        }
+    }
+}
