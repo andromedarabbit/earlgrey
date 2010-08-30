14e6bc2850a6c7881b7a0bf58ede1ce872c09fe4
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-08-30 08:51:07 +0000

    VS2008에 맞게 닷넷 프레임워크 버전 조정

--- src/MSBuild_x64.bat
+++ src/MSBuild_x64.bat
@@ -1,6 +1,6 @@
 @echo off
 SETLOCAL
 call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x64 >> _temp.txt
-call "C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" %*
+call "C:\WINDOWS\Microsoft.NET\Framework64\v3.5\msbuild.exe" %*
 SET ERR_LEVEL=%errorlevel%
 exit /b %ERR_LEVEL%
