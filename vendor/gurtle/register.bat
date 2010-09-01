bceec7e5fd20830709b2878f9344a6c44a302c03
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-09-01 02:49:53 +0000

    (Closes issue #84) : Gurtle 의 google code issues 연동 스크립트 자동 등록하기

--- vendor/gurtle/register.bat
+++ vendor/gurtle/register.bat
@@ -1,9 +1,5 @@
-@echo on
-SETX /S %COMPUTERNAME% GURTLE_SCRIPT "%~dp0scripts\updissue.cmd"
-SET ERR_LEVEL=%errorlevel%
-if %ERR_LEVEL% NEQ 0 exit /b %ERR_LEVEL%
-
-SETX /S %COMPUTERNAME% GURTLE_ISSUE_UPDATE_CMD "%GURTLE_SCRIPT% --username {username} --password {password} --project {project} --issue {issue.id} --status {status} --comment {comment} --debug"
+@echo off
+SETX /S "%COMPUTERNAME%" GURTLE_ISSUE_UPDATE_CMD "\"%~dp0scripts\updissue.cmd\" --username {username} --password {password} --project {project} --issue {issue.id} --status {status} --comment {comment} --debug"
 SET ERR_LEVEL=%errorlevel%
 if %ERR_LEVEL% NEQ 0 exit /b %ERR_LEVEL%
 
