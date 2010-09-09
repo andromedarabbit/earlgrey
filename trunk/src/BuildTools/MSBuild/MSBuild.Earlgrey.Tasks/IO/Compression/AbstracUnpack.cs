b1855cfce519110b96442510c2ff0d1b0909d2b2
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-09-09 15:09:03 +0000

    로컬 복사본이 꼬여서 커밋 안 된 파일이 많습니다.

---
+++ trunk/src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks/IO/Compression/AbstracUnpack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Ionic.Zip;
+using Microsoft.Build.Framework;
+
+namespace MSBuild.Earlgrey.Tasks.IO.Compression
+{
+    public abstract class AbstractUnpack : AbstractTask
+    {
+        protected AbstractUnpack()
+        {
+            Overwrite = false;
+        }
+
+        protected override bool ValidateParameters()
+        {
+            if (TaskItemHelper.FileExists(ZipFilePath) == false)
+            {
+                Log.LogError(
+                    string.Format("Source file path '{0}' not found!", ZipFilePath.ItemSpec)
+                    );
+                return false;
+            }
+
+            if (TaskItemHelper.FolderExists(TargetDir) == true && Overwrite == false)
+            {
+                Log.LogError(
+                    string.Format("Target directory path '{0}' already exists!", TargetDir.ItemSpec)
+                    );
+                return false;
+            }
+
+            return true;
+        }
+
+        protected abstract override bool ExecuteCommand();
+
+        [Required]
+        public abstract ITaskItem TargetDir { get; set; }
+
+        protected string InternalTargetDir
+        {
+            get { return TaskItemHelper.GetFullPath(TargetDir); }
+        }
+
+        public abstract bool Overwrite { get; set; }
+
+        // TODO: 중복 코드 제거하기 (Pack 확인)
+        [Required]
+        public abstract ITaskItem ZipFilePath { get; set; }
+
+        protected string InternalZipFilePath
+        {
+            get { return TaskItemHelper.GetFullPath(ZipFilePath); }
+        }
+
+        
+    }
+}
