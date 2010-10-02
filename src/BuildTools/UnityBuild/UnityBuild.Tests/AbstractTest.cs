fe3efdc303a6edc337c27498014d502ca1938e45
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-10-02 12:49:22 +0000

    참 단위테스트 코드를 빼먹었네요.

---
+++ src/BuildTools/UnityBuild/UnityBuild.Tests/AbstractTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Earlgrey;
+using NUnit.Framework;
+
+namespace UnityBuild.Tests
+{
+    public class AbstractTest
+    {
+        private static readonly string _TempDir;
+        private static readonly string _ThisDir;
+
+        static AbstractTest()
+        {
+            _ThisDir = Path.Combine(
+                TaskUtility.ThisAssemblyDirectory
+                , @"UnityBuildTestResources"
+                );
+
+            _TempDir = Path.Combine(_ThisDir, "Temp");
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            if(Directory.Exists(_TempDir))
+                Directory.Delete(_TempDir, true);
+
+            Directory.CreateDirectory(_TempDir);
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            if (Directory.Exists(_TempDir))
+                Directory.Delete(_TempDir, true);
+        }
+
+        public static string TempDir
+        {
+            get { return _TempDir; }
+        }
+
+        public static string ThisDir
+        {
+            get { return _ThisDir; }
+        }
+    }
+}
