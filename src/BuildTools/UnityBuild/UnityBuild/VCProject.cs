536e832c14157f04740a945ddb1591b0249f88e5
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-10-03 16:56:36 +0000

    제대로 된 형태를 갖추고 올리면 좋은데 시간이 없네요. 일단 커밋!

---
+++ src/BuildTools/UnityBuild/UnityBuild/VCProject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using CWDev.SLNTools.Core;
+
+namespace UnityBuild
+{
+    internal class VcProject
+    {
+        private readonly Project _project;
+
+        public VcProject(Project project)
+        {
+            Debug.Assert(project != null);
+            Debug.Assert(
+                project.ProjectTypeGuid == KnownProjectTypeGuid.VisualC
+                );
+            _project = project;
+        }
+
+        public void Run()
+        {
+            // SolutionFile slnFile = SolutionFile.FromFile(solutionFileFullPath);
+            using (TextReader tr = new StreamReader(_project.FullPath))
+            {
+                // Now deserialize.
+                var project = (VisualStudioProjectType)(
+                    new XmlSerializer(typeof(VisualStudioProjectType))
+                ).Deserialize(tr);
+
+                Debug.Assert(project != null);
+            }
+        }
+
+
+    }
+}
