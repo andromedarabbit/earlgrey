1be29f996fc6e58ac675e297f75be93968d9d71a
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-10-05 17:22:07 +0000

    소스파일인지 판단하는 자료를 설정 파일에 넣는다.

---
+++ src/BuildTools/UnityBuild/UnityBuild/CustomExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityBuild
+{
+    internal static class CustomExtensions
+    {
+        public static bool IsSrcFile(this FileType file)
+        {
+            return Properties.UnityBuild.Default.IsSourceFile(file.RelativePath);
+        }
+    }
+}
