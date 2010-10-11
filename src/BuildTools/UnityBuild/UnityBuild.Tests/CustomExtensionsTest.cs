21e2f6163f734bb8d1a88e39d15dfc5910d1b3e5
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-10-11 17:12:43 +0000

    1. 컴파일하지 않는 소스코드인지 확인하는 IsExcludedFromBuild 메서드의 단위 테스트 2. 단위 테스트마다 쓰는 메서드 GetEarlgreyProject 를 ExtractMethod 함.

--- src/BuildTools/UnityBuild/UnityBuild.Tests/CustomExtensionsTest.cs
+++ src/BuildTools/UnityBuild/UnityBuild.Tests/CustomExtensionsTest.cs
@@ -1,18 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using CWDev.SLNTools.Core;
 using NUnit.Framework;
 
 namespace UnityBuild.Tests
 {
     [TestFixture]
-    public class CustomExtensionsTest
+    public class CustomExtensionsTest : AbstractTest
     {
         [Test]
-        public void TT()
+        public void IsExcludedFromBuildTest()
         {
-            
+            Project earlgreyProject = GetEarlgreyProject();
+
+            var vcProject = new VcProject(earlgreyProject);
+            vcProject.Initialize();
+
+            var details = vcProject.Details;
+
+            var filters = from fileOrFilter in details.Files
+                         where fileOrFilter is FilterType
+                         select (FilterType)fileOrFilter;
+
+            var sourceFileFilter = filters.Where(filter => filter.Name == "Source Files").First();
+            var sourceFileSubFilters = from fileOrFilter in sourceFileFilter.Items
+                                       where fileOrFilter is FilterType
+                                       select (FilterType)fileOrFilter; 
+          
+            var excludedFilter = sourceFileSubFilters.Where(filter => filter.Name == "UnityBuildTest-Excluded").First();
+
+            var throwErrorCppResult = from file in excludedFilter.Items
+                                where file is FileType
+                                select (FileType) file
+                                ;
+            FileType throwErrorCpp = throwErrorCppResult.First();
+            Assert.IsTrue(throwErrorCpp.IsExcludedFromBuild());
         }
     }
 }
