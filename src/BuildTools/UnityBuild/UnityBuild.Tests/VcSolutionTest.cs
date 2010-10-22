2aea8a4359ea323d60a825a477d88207eeba3d74
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-10-22 13:10:19 +0000

    방금 빠진 파일...

---
+++ src/BuildTools/UnityBuild/UnityBuild.Tests/VcSolutionTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CWDev.SLNTools.Core;
+using NUnit.Framework;
+
+namespace UnityBuild.Tests
+{
+    [TestFixture]
+    public class VcSolutionTest // : AbstractTest
+    {
+        [Test]
+        public void LoadTest()
+        {
+            var vcSolution = new VcSolution(AbstractTest.SolutionFilePath);
+            vcSolution.Load();
+
+
+        }
+
+        [Test]
+        public void CopyPlatformConfiguration()
+        {
+            var vcSolution = new VcSolution(AbstractTest.SolutionFilePath);
+            vcSolution.Load();
+
+            string srcPlatformConfigurationName = "Debug|Win32";
+            // string srcPlatformConfigurationValue = "Debug|Win32";
+
+            var configurationPlatforms = vcSolution.ConfigurationPlatforms;
+            Assert.IsTrue(
+                vcSolution.HasConfigurationPlatform(srcPlatformConfigurationName)
+                );
+
+
+            string dstPlatformConfigurationName = "Debug-UnityBuild|Win32";
+            string dstPlatformConfigurationValue = "Debug-UnityBuild|Win32";
+
+            Assert.IsFalse(
+                vcSolution.HasConfigurationPlatform(dstPlatformConfigurationName)
+                );
+
+            var srcPlatformConfiguration = configurationPlatforms[srcPlatformConfigurationName];
+            var dstPlatformConfiguration = new PropertyLine(dstPlatformConfigurationName, dstPlatformConfigurationValue);
+
+            configurationPlatforms.Add(dstPlatformConfiguration);
+
+            vcSolution.Save();
+        }
+
+
+
+    }
+}
+
---
+++ src/BuildTools/UnityBuild/UnityBuild/VcSolution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using CWDev.SLNTools.Core;
+
+namespace UnityBuild
+{
+    public class VcSolution
+    {
+        private readonly string _solutionFilePath;
+        private SolutionFile _summary;
+        private readonly List<VcProject> _vcProjects;
+
+        public VcSolution(string solutionFilePath)
+        {            
+            _solutionFilePath = solutionFilePath;
+            _vcProjects = new List<VcProject>();
+        }
+
+        public SolutionFile Summary
+        {
+            get 
+            {
+                Debug.Assert(_summary != null);
+                return _summary;
+            }
+        }
+
+        public void Load()
+        {
+            _summary = SolutionFile.FromFile(_solutionFilePath);
+
+            foreach(var projectSummary in _summary.Projects)
+            {
+                if (projectSummary.ProjectTypeGuid != KnownProjectTypeGuid.VisualC)
+                    continue;
+
+                var vcProject = new VcProject(projectSummary);
+                vcProject.Load();
+
+                _vcProjects.Add(vcProject);
+            }
+        }
+
+
+        public void Save()
+        {
+            foreach (var project in _vcProjects)
+            {
+                project.Save();
+            }
+            _summary.Save();
+        }
+
+        public PropertyLineHashList ConfigurationPlatforms
+        {
+            get
+            {
+                return this.Summary.GlobalSections["SolutionConfigurationPlatforms"].PropertyLines;
+            }
+        }
+
+        public bool HasConfigurationPlatform(string configurationPlatformName)
+        {
+            return ConfigurationPlatforms.Contains(configurationPlatformName);
+        }
+    }
+}
