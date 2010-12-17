13781ec698a5878626f36af106fd79d16e264bb0
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-12-17 16:46:58 +0000

    (Re issue #93) : Unity build 개발 일지 커밋할 때 이슈와 연동하는 걸 자꾸 잊네. 소스 코드를 한데 합쳐서 Unity Build용 소스 코드를 만드는 기능을 구현 하는 중이다. 졸려서 오늘은 여기까지.

---
+++ trunk/src/BuildTools/UnityBuild/UnityBuild/FilesMerge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace UnityBuild
+{
+    internal class FilesMerge
+    {
+        private readonly IEnumerable<FileType> _files;
+        private readonly Dictionary<string, FileType> _filesByPath;
+
+        public FilesMerge(IEnumerable<FileType> files)
+        {
+            Debug.Assert(_files != null);
+            _files = files;
+            _filesByPath = _files.ToDictionary(item => item.RelativePath);
+        }
+
+        public void Merge()
+        {
+
+        }
+    }
+}
