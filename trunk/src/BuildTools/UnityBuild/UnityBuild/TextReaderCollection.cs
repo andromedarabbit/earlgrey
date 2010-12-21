7d2172c4f9eec262aa8da45b3f1f08b149331a1b
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-12-21 15:31:56 +0000

    방금 빠진 파일

---
+++ trunk/src/BuildTools/UnityBuild/UnityBuild/TextReaderCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UnityBuild
+{
+
+    internal class TextReaderCollection : IEnumerable<TextReader>, IDisposable
+    {
+        private readonly TextReaders _internalCollection;
+
+        public TextReaderCollection(TextFile[] srcFiles, string delimiter)
+        {
+            _internalCollection = new TextReaders(srcFiles, delimiter);
+        }
+
+        public IEnumerator<TextReader> GetEnumerator()
+        {
+            return _internalCollection;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _internalCollection.Dispose();
+        }
+    }
+
+}
