cf544c649479e705a3ebb8b1f50990e0642d83f1
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2010-12-27 07:19:38 +0000

    1. 안 쓰는 파일 VisualStudioProjectType.cs 삭제 2. 폴더 경로 별로 묶는 기능을 Linq 를 이용해 다시 구현함

---
+++ trunk/src/BuildTools/UnityBuild/UnityBuild/ExtendedFileType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityBuild
+{
+    public class ExtendedFileType : FileType
+    {
+        private readonly PrecompiledHeaderOptions _precompiledHeaderOptions;
+
+     
+        public ExtendedFileType(PrecompiledHeaderOptions precompiledHeaderOptions)
+            : base()
+        {
+            this._precompiledHeaderOptions = precompiledHeaderOptions;
+        }
+
+        public PrecompiledHeaderOptions PrecompiledHeaderOption
+        {
+            get { return _precompiledHeaderOptions; }
+            // set { _precompiledHeaderOptions = value; }
+        }
+    }
+}
