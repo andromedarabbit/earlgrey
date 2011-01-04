5be33558a719fdc57b0dc601d6e531cca2129296
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-01-04 09:45:41 +0000

    Visual Studio 솔루션과 그 하위 프로젝트의 복사본(파일 복사)을 만드는 기능을 구현하려 함.

---
+++ src/BuildTools/UnityBuild/UnityBuild/ProjectFileNameConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UnityBuild
+{
+    public class ProjectFileNameConverter : IProjectFileNameConverter 
+    {
+        private const string Suffix = "-UnityBuild";
+        private static readonly string[] ExtensionsAllowed;
+
+        static ProjectFileNameConverter()
+        {
+            ExtensionsAllowed = new string[] {".vcproj", ".sln"};
+        }
+
+        public virtual bool IsNewName(string name)
+        {
+            Debug.Assert(string.IsNullOrEmpty(name) == false);
+
+            string extension = Path.GetExtension(name);
+            if(string.IsNullOrEmpty(extension))
+                throw new ArgumentException();
+
+            // if(ExtensionsAllowed.Contains())
+
+
+            // return name.EndsWith(Suffix, StringComparison.CurrentCultureIgnoreCase);
+            throw new NotImplementedException();
+        }
+
+        public virtual string GetNewName(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException();
+
+            if (name.Length == 0)
+                throw new ArgumentException();
+
+            return name + Suffix;
+        }
+
+        public virtual string GetOldName(string name)
+        {
+            if (name.EndsWith(Suffix) == false)
+                throw new ArgumentException();
+
+            return name + Suffix;
+        }
+    }
+}
