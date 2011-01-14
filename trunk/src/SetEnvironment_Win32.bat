bf51e0cdbfe18c40610e30be51a1c331e44908b5
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-01-14 16:36:21 +0000

    Update issue 103 PDB 파일에 저장소 정보를 인덱싱하기 1. 필요한 도구 (Perl, Debugging Tools For Windows) 2. 인덱싱용 빌드 스크립트
    일단 x64만 지원함.
    x86은 32비트 윈도우에서 작업해서 올릴 예정임

---
+++ trunk/src/SetEnvironment_Win32.bat
@@ -0,0 +1,6 @@
+@echo off
+SET PATH=%PATH%;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable\perl\bin
+
+call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+SET ERR_LEVEL=%errorlevel%
+exit /b %ERR_LEVEL%
