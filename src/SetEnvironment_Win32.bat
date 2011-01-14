3c0fc9097ab71bec8d3c5754254d120f16467da4
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-01-14 17:47:10 +0000

    Update issue 103 * x86 용 Debugging Tools for Windows * 압축 파일로 묶어서 보이지 않겠지만 잘못된 정규식 수정 (svn.pm) * vendor 에 있는 svn.exe 를 쓰도록 경로 설정

--- src/SetEnvironment_Win32.bat
+++ src/SetEnvironment_Win32.bat
@@ -1,5 +1,5 @@
 @echo off
-SET PATH=%PATH%;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable\perl\bin
+SET PATH=%PATH%;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable\perl\bin;%~dp0..\vendor\BuildTool\SubversionClient\v1.6.13\CollabNet\Subversion Client
 
 call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 SET ERR_LEVEL=%errorlevel%
