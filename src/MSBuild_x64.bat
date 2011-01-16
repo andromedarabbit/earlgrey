fe35056aa3a3425a3a40f6c29cbdb0b620143a67
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-01-16 06:01:23 +0000

    1. 작업 폴더가 배치파일이 있는 곳이 아닐수도 있기 때문에 파일의 경로를 정확하게 명시함 2. 새 빌드 스크립트 등을 솔루션에 추가함

--- src/MSBuild_x64.bat
+++ src/MSBuild_x64.bat
@@ -1,6 +1,6 @@
 @echo off
 SETLOCAL
-call "SetEnvironment_x64.bat"
+call "%~dp0SetEnvironment_x64.bat"
 call "C:\WINDOWS\Microsoft.NET\Framework64\v3.5\msbuild.exe" %*
 SET ERR_LEVEL=%errorlevel%
 exit /b %ERR_LEVEL%
