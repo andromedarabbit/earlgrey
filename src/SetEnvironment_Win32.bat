e52d45839f3bf61faf548c4812807960457cb241
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-01-18 15:18:28 +0000

    Perl 압축파일의 경로를 바꾼다. SVN의 Sparse checkout 기능을 활용한 스크립트가 곧 올라갈 것인데, 빌드 도구를 따로 모아야 관리하기 편하다.

--- src/SetEnvironment_Win32.bat
+++ src/SetEnvironment_Win32.bat
@@ -1,5 +1,5 @@
 @echo off
-SET PATH=%~dp0..\vendor\strawberry-perl-5.12.1.0-portable;%~dp0..\vendor\strawberry-perl-5.12.1.0-portable\perl\bin;%~dp0..\vendor\BuildTool\SubversionClient\v1.6.13\CollabNet\Subversion Client;%PATH%
+SET PATH=%~dp0..\vendor\BuildTool\strawberry-perl-5.12.1.0-portable;%~dp0..\vendor\BuildTool\strawberry-perl-5.12.1.0-portable\perl\bin;%~dp0..\vendor\BuildTool\SubversionClient\v1.6.13\CollabNet\Subversion Client;%PATH%
 
 call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 SET ERR_LEVEL=%errorlevel%
