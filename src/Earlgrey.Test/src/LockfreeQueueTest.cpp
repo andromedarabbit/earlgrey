39cd4a348bb2b3054a1c0f861d03d077ef2d65e3
gwkang76 <gwkang76@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-01-23 11:25:18 +0000

    lockfree queue 의 MoveTail 메서드를 Dequeue 메서드에 포함시키고 제거

--- src/Earlgrey.Test/src/LockfreeQueueTest.cpp
+++ src/Earlgrey.Test/src/LockfreeQueueTest.cpp
@@ -29,68 +29,105 @@ namespace Earlgrey
 		class LockFreeQueueThread : public SimpleThread
 		{
 		public:
-			LockFreeQueueThread() : m_push(true)
+			enum {
+				SINGLE_PUSH_POP,
+				ROLE_BASED
+			};
+			LockFreeQueueThread() : TestType(ROLE_BASED)
 			{
 			}
+
+		public:
+			int TestType;
+			static volatile LONG s_fail_count;
 
 		private:
 			enum { MAX_VALUE = 10000 };
 
 			DWORD Run()
 			{
+				bool push_role = InterlockedIncrement( &s_push ) % 2 == 0;
 				while(IsRunning())
 				{
-					LONG value = 0L;
-
-					if (m_push)
+					if (ROLE_BASED == TestType)
 					{
-						value = InterlockedIncrement( &s_value );
-						s_container.Enqueue( value );
-
-						if (value > MAX_VALUE)
-						{
-							m_push = false;
-						}
-						continue;
+						RoleBased(push_role);
 					}
-
-					s_container.MoveTail();
-
-					if (!s_container.Dequeue( value ))
+					else if (SINGLE_PUSH_POP == TestType)
 					{
-						break;
+						push_role = !push_role;
+						SinglePushPop(push_role);
 					}
-
-					DoSomething();
+					
 				}
 				return 0;
 			}
 
-			void DoSomething()
+			void SinglePushPop(bool push_role)
 			{
-				InterlockedDecrement( &s_value );
+				LONG value = 0L;
+				if (push_role)
+				{
+					s_container.Enqueue( value );
+					return;
+				}
+				
+				//s_container.MoveTail();
+				if (!s_container.Dequeue( value ))
+				{
+					InterlockedIncrement( &s_fail_count );
+				}
 			}
 
-			bool m_push;
+			void RoleBased( bool push_role )
+			{
+				LONG value = 0L;
+				if (push_role)
+				{
+					value = InterlockedIncrement( &s_value );
+					s_container.Enqueue( value );
+					return;
+				}
+
+				//s_container.MoveTail();
+
+				if (!s_container.Dequeue( value ))
+				{
+					InterlockedIncrement( &s_fail_count );
+				}
+
+				DoSomething();
+			}
+
+			void DoSomething()
+			{
+				//InterlockedDecrement( &s_value );
+			}
+
+			static volatile LONG s_push;
 
 			static volatile LONG s_value;
 			static Algorithm::Lockfree::Queue<LONG> s_container;
+			
 		};
 
 		volatile LONG LockFreeQueueThread::s_value;
+		volatile LONG LockFreeQueueThread::s_push = 0;
+		volatile LONG LockFreeQueueThread::s_fail_count = 0;
 		Algorithm::Lockfree::Queue<LONG> LockFreeQueueThread::s_container;
 
 		TEST(LockfreeQueueTest, ContentionTest)
 		{
-			enum { MAX_THREADS = 2 };
+			enum { MAX_THREADS = 15 };
 			LockFreeQueueThread QueueTestThread[MAX_THREADS];
 
 			for (int i=0; i < MAX_THREADS; i++)
 			{
+				QueueTestThread[i].TestType = LockFreeQueueThread::SINGLE_PUSH_POP;
 				ASSERT_EQ( QueueTestThread[i].Create(), TRUE );
 			}
 
-			Sleep( 1000 );
+			Sleep( 5000 );
 
 			for (int i=0; i < MAX_THREADS; i++)
 			{
@@ -101,6 +138,8 @@ namespace Earlgrey
 			{
 				QueueTestThread[i].Join();
 			}
+
+			EXPECT_GT( 1, LockFreeQueueThread::s_fail_count );
 		}
 	}
 }
