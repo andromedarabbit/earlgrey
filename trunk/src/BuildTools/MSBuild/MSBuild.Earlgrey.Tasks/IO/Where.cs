762cdb31ec06874e17efe65aecafd679923cdb39
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-04-21 13:09:40 +0000

    1. 환경변수 PATH 에 지정된 경로와 현재 디렉터리에서 지정한 파일이 있는지 검사하는 MSBuild 확장기능 Where 2. IncrediBuild 가 로컬 머신에 설치된 경우에 사용자가 특별히 지정하지 않으면 Visual Studio 대신 IncrediBuild 로 빌드하게 수정함.

---
+++ trunk/src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks/IO/Where.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Earlgrey;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using MSBuild.Community.Tasks.Services;
+
+namespace MSBuild.Earlgrey.Tasks.IO
+{
+    public class Where : AbstractTask
+    {
+        private readonly List<ITaskItem> _pathsFound;
+
+        public Where()
+        {
+            _pathsFound = new List<ITaskItem>();
+        }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Output]
+        public ITaskItem[] PathsFound 
+        { 
+            get { return _pathsFound.ToArray(); }
+        }
+    
+        protected override bool ValidateParameters()
+        {
+            if (string.IsNullOrEmpty(FileName))
+            {
+                Log.LogError("FileName is empty!");
+                return false;
+            }
+            return true;
+        }
+
+        protected override bool ExecuteCommand()
+        {
+            IEnumerable<string> pathsFound = FileSearch.FindAll(FileName);
+            foreach (var path in pathsFound)
+            {
+                Debug.Assert(string.IsNullOrEmpty(path) == false);
+                _pathsFound.Add(new TaskItem(path));
+            }
+
+            return true;
+        }
+    }
+}
