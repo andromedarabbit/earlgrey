c6e0532194004fcbfc825249db03fd21281bba12
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-04-28 14:43:16 +0000

    콘솔 출력 기능 개발 중

---
+++ src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks/BetterMessage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSBuild.Earlgrey.Tasks
+{
+    public class BetterMessage : AbstractTask
+    {
+        public string Text { get; set; }
+        public string Color { get; set; }
+
+
+        private ConsoleColor InternalColor
+        {
+            get
+            {
+                return (ConsoleColor)Enum.Parse(typeof (ConsoleColor), Color, true);
+            }
+        }
+
+        protected override bool ValidateParameters()
+        {            
+            return true;
+        }
+
+        protected override bool ExecuteCommand()
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                Console.WriteLine();
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(Color))
+            {
+                Console.WriteLine(Text);
+                return true;
+            }
+
+            ConsoleColor oldColor = Console.ForegroundColor;
+            try
+            {
+                Console.ForegroundColor = InternalColor;
+                Console.WriteLine(Text);
+            }
+            catch (ArgumentException argEx)
+            {
+                Log.LogErrorFromException(argEx);
+                return false;
+            }
+            finally
+            {
+                Console.ForegroundColor = oldColor;
+            }
+            
+            return true;
+        }
+    }
+
+}
