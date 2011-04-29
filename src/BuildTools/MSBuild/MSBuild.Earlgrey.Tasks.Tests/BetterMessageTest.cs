338e9ed6e5d62a0601be9e43c9bbc1fe2b6528a2
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-04-29 04:03:19 +0000

    콘솔 출력하는 기능 + 그 단위 테스트

---
+++ src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks.Tests/BetterMessageTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Earlgrey;
+using NUnit.Framework;
+
+namespace MSBuild.Earlgrey.Tasks.Tests
+{
+    [TestFixture]
+    public class BetterMessageTest
+    {
+ 
+        [Test]
+        public void Print()
+        {
+            string localPath = Path.GetFullPath(
+                Path.Combine(TaskUtility.ThisAssemblyDirectory, @"..\..\")
+                );
+
+            BetterMessage diff = new BetterMessage();
+            diff.BuildEngine = new MockBuildEngine();
+            diff.Text = "This is a test.";            
+            Assert.IsTrue(diff.Execute());
+        }
+
+       
+    }
+}
