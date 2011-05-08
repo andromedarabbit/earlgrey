df93657ee66395ea2d05bf436f0755fc11659e40
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-05-08 14:23:44 +0000

    [MSBuild 확장기능] WinSCP 기능 추가 중

---
+++ src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks.Tests/Net/WinScpTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MSBuild.Earlgrey.Tasks.Net;
+using MSBuild.Earlgrey.Tasks.Subversion;
+using NUnit.Framework;
+
+namespace MSBuild.Earlgrey.Tasks.Tests.Net
+{
+    [TestFixture]
+    public class WinScpTest
+    {
+        [Ignore]
+        [Test]
+        public void UsingSFTP()
+        {
+            WinScp instance = new WinScp();
+            instance.BuildEngine = new MockBuildEngine();
+            instance.HostName = "andromedarabbit.net";
+            instance.ProtocolSFtp = true;
+            instance.Password = "";
+            instance.UserName = "";
+            instance.HostKey = "ssh-rsa 2048 a4:d0:ec:7a:32:33:4e:43:2d:d2:c9:e8:ca:d0:42:c0";
+            instance.ScriptText = "cd /home";
+            Assert.IsTrue(instance.Execute());
+        }
+    }
+}
