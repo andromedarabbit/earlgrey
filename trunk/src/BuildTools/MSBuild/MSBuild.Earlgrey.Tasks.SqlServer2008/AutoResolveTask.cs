01c9a18907f6d63773653e5bf0c732fc4c838e62
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-05-20 07:23:57 +0000

    AssemblyResolve 이벤트 처리기를 잘못 구현해서 고침

--- trunk/src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks.SqlServer2008/AutoResolveTask.cs
+++ trunk/src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks.SqlServer2008/AutoResolveTask.cs
@@ -29,35 +29,19 @@ namespace MSBuild.Earlgrey.Tasks.SqlServer2008
         {
             //This handler is called only when the common language runtime tries to bind to the assembly and fails.
 
-            //Retrieve the list of referenced assemblies in an array of AssemblyName.
-            Assembly MyAssembly, objExecutingAssemblies;
-            string strTempAssmbPath = "";
+            string assemblyNameMissing = args.Name.Substring(0, args.Name.IndexOf(","));
 
-            objExecutingAssemblies = Assembly.GetExecutingAssembly();
-            AssemblyName[] arrReferencedAssmbNames = objExecutingAssemblies.GetReferencedAssemblies();
+            string assemblyPath = Path.Combine(ThisAssemblyDirectory, @"ExternalLibs\Microsoft SQL Server 2008 Management Objects");
+            assemblyPath = Path.Combine(assemblyPath, assemblyNameMissing + ".dll");
 
-            //Loop through the array of referenced assembly names.
-            foreach (AssemblyName strAssmbName in arrReferencedAssmbNames)
-            {
-                //Check for the assembly names that have raised the "AssemblyResolve" event.
-                if (strAssmbName.FullName.Substring(0, strAssmbName.FullName.IndexOf(",")) == args.Name.Substring(0, args.Name.IndexOf(",")))
-                {
-                    //Build the path of the assembly from where it has to be loaded.				
-                    // strTempAssmbPath = "C:\\Myassemblies\\" + args.Name.Substring(0, args.Name.IndexOf(",")) + ".dll";
+            if (File.Exists(assemblyPath) == false)
+                return null;
 
-                    strTempAssmbPath = Path.Combine(ThisAssemblyDirectory, @"ExternalLibs\Microsoft SQL Server 2008 Management Objects");
-                    strTempAssmbPath = Path.Combine(strTempAssmbPath, args.Name.Substring(0, args.Name.IndexOf(",")) + ".dll");
-
-
-                    break;
-                }
-
-            }
             //Load the assembly from the specified path. 					
-            MyAssembly = Assembly.LoadFrom(strTempAssmbPath);
+            Assembly assemblyFound = Assembly.LoadFrom(assemblyPath);
 
             //Return the loaded assembly.
-            return MyAssembly;	
+            return assemblyFound;	
         }
     }
 }
