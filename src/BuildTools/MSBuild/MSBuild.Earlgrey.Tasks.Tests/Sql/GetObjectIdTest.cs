1cb661b04216228e6d57407b0962575d1a53e077
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-05-26 09:45:26 +0000

    [SQL] OBJECT_ID 와 USER_ID 를 MSBuild 확장기능으로 사용한다.

---
+++ src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks.Tests/Sql/GetObjectIdTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using MSBuild.Earlgrey.Tasks.Sql;
+using NUnit.Framework;
+
+namespace MSBuild.Earlgrey.Tasks.Tests.Sql
+{
+    [TestFixture]
+    public class GetObjectIdTest
+    {
+        [Test]
+        public void Run()
+        {
+            GetObjectId instance = new GetObjectId();
+            instance.BuildEngine = new MockBuildEngine();
+            instance.ConnectionString = @"Data Source=localhost\SQLEXPRESS; Initial Catalog=master; Integrated Security=SSPI";
+            
+            List<ITaskItem> names = new List<ITaskItem>();
+            names.Add(new TaskItem("INFORMATION_SCHEMA.TABLES"));
+            names.Add(new TaskItem("INFORMATION_SCHEMA.VIEWS"));
+
+            instance.Names = names.ToArray();
+
+            Assert.IsTrue(instance.Execute());
+            
+            ITaskItem[] identities = instance.Ids;
+            CollectionAssert.IsNotEmpty(identities);
+
+            Assert.AreEqual(names.Count, identities.Length);
+
+            Assert.IsTrue(
+                identities.All(item => int.Parse(item.GetMetadata("Id")) != 0)
+                );
+        }
+    }
+}
---
+++ src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks/Sql/AbstractGetId.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Earlgrey.Tasks.Sql
+{
+    public abstract class AbstractGetId : AbstractTask
+    {
+        private readonly List<ITaskItem> _identities;
+
+
+        [Required]
+        public string ConnectionString { get; set; }
+
+        [Required]
+        public ITaskItem[] Names { get; set; }
+
+        [Output]
+        public ITaskItem[] Ids
+        {
+            get { return _identities.ToArray(); }
+        }
+
+        protected AbstractGetId()
+        {
+            _identities = new List<ITaskItem>();
+        }
+
+        protected override bool ValidateParameters()
+        {
+            if (Names.Any(item => string.IsNullOrEmpty(item.ItemSpec) == true))
+            {
+                Log.LogError("Every object name should have a name!");
+                return false;
+            }
+
+            return true;
+        }
+
+
+        protected abstract string GetCommandString(ITaskItem item);
+        
+            // return string.Format("SELECT OBJECT_ID(N'{0}')", name.ItemSpec);
+        
+
+        protected override bool ExecuteCommand()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    foreach (ITaskItem name in Names)
+                    {
+                        string commandStr = GetCommandString(name);
+                        SqlCommand command = new SqlCommand(commandStr, connection);
+
+                        object returnValue = command.ExecuteScalar();
+                        string objectId = returnValue == null ? string.Empty : returnValue.ToString();
+
+                        ITaskItem newItem = new TaskItem(name);
+                        newItem.SetMetadata("Id", objectId);
+                        _identities.Add(newItem);
+                    }
+
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
