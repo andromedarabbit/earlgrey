21c02893b0f0fce078569319e00d0e19a256e77f
kaistizen <kaistizen@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-05-26 09:45:26 +0000

    [SQL] OBJECT_ID 와 USER_ID 를 MSBuild 확장기능으로 사용한다.

---
+++ trunk/src/BuildTools/MSBuild/MSBuild.Earlgrey.Tasks/Sql/GetUserId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace MSBuild.Earlgrey.Tasks.Sql
+{
+    public class GetUserId : AbstractGetId
+    {
+        public GetUserId()
+            : base()
+        {
+
+        }
+
+        protected override string GetCommandString(ITaskItem item)
+        {
+            return string.Format("SELECT USER_ID(N'{0}')", item.ItemSpec);
+        }
+
+    }
+}
