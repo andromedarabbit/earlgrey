15711be6e6269ab2870b535a73f9fe90cd27f4da
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-09-18 05:50:12 +0000

    BASE64 인코딩 구현 다듬는 중

--- src/Earlgrey/inc/Convert.h
+++ src/Earlgrey/inc/Convert.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Uncopyable.h"
+#include "txstring.h"
 
 namespace Earlgrey
 {
@@ -10,12 +11,58 @@ namespace Earlgrey
 
 	public:
 		// \ref http://www.iamcorean.net/130
-		static 
-			int FromBase64(const char * const text, BYTE * bytes[], size_t& numBytes);
+		// Decpde
+		static
+			size_t GetMinimumBytesForDecode(size_t textLength);
 
+		static 
+			int FromBase64(const char * const text, size_t textLength, BYTE * bytes, size_t numBytes);
+	/*	static
+			int FromBase64(const xstring& text, BYTE * bytes, size_t numBytes);*/
+
+		/*template<class _Elem, class _Traits, class _Ax>
+		static 
+			int FromBase64(const std::basic_string<_Elem, _Traits, _Ax> & text, BYTE * bytes, size_t numBytes)
+		{
+			return FromBase64(text.c_str(), text.length(), bytes, numBytes);
+		}*/
+
+		template<class _Elem, class _Traits, class _Ax>
+		static 
+			int FromBase64(const std::basic_string<_Elem, _Traits, _Ax> & text, BYTE * bytes, size_t numBytes);
+
+		template<class _Ax>
+		static 
+			int FromBase64(const std::basic_string<char, std::char_traits<char>, _Ax> & text, BYTE * bytes, size_t numBytes)
+		{
+			return FromBase64(text.c_str(), text.length(), bytes, numBytes);
+		}
+		
+		/*static 
+			int FromBase64(const WCHAR * const text, size_t textLength, BYTE * bytes, size_t numBytes);
+		*/	
+		// Encode
+		static
+			size_t GetMinimumLengthForEncode(size_t numBytes);
 
 		static
-			size_t ToBase64(BYTE * const bytes, size_t numBytes, char * encodedText[], size_t size = 0);
+			size_t ToBase64(const BYTE * const bytes, size_t numBytes, char encodedText[], size_t textLength);
+		template<class _Elem, class _Traits, class _Ax>
+		static
+			size_t ToBase64(const BYTE * const bytes, size_t numBytes, const std::basic_string<_Elem, _Traits, _Ax> & encodedText);
+
+		template<class _Ax>
+		static 
+			size_t ToBase64(const BYTE * const bytes, size_t numBytes, const std::basic_string<char, std::char_traits<char>, _Ax> & encodedText)
+		{
+			const size_t textLength = GetMinimumLengthForEncode(numBytes);
+			encodedText.reserve(textLength);
+			return ToBase64String(bytes, numBytes, encodedText, textLength);
+		}
+		/*static
+			size_t ToBase64(const BYTE * const bytes, size_t numBytes, xstring& encodedText);*/
+		/*static
+			size_t ToBase64(const BYTE * const bytes, size_t numBytes, WCHAR encodedText[], size_t textLength);*/
 	};
 	
 
