3608c34f7ba6a344ae4809f669b45c35b8ee5135
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-09-19 13:05:07 +0000

    base64 인코딩 다듬는 중

--- src/Earlgrey/inc/Convert.h
+++ src/Earlgrey/inc/Convert.h
@@ -17,19 +17,9 @@ namespace Earlgrey
 
 		static 
 			size_t FromBase64(const char * const text, size_t textLength, BYTE * bytes, size_t numBytes);
-	/*	static
-			int FromBase64(const xstring& text, BYTE * bytes, size_t numBytes);*/
-
-		/*template<class _Elem, class _Traits, class _Ax>
 		static 
-			int FromBase64(const std::basic_string<_Elem, _Traits, _Ax> & text, BYTE * bytes, size_t numBytes)
-		{
-			return FromBase64(text.c_str(), text.length(), bytes, numBytes);
-		}*/
-
-		template<class _Elem, class _Traits, class _Ax>
-		static 
-			size_t FromBase64(const std::basic_string<_Elem, _Traits, _Ax> & text, BYTE * bytes, size_t numBytes);
+			size_t FromBase64(const WCHAR * const text, size_t textLength, BYTE * bytes, size_t numBytes);
+	
 
 		template<class _Ax>
 		static 
@@ -38,18 +28,16 @@ namespace Earlgrey
 			return FromBase64(text.c_str(), text.length(), bytes, numBytes);
 		}
 		
-		/*static 
-			int FromBase64(const WCHAR * const text, size_t textLength, BYTE * bytes, size_t numBytes);
-		*/	
+	
 		// Encode
 		static
 			size_t GetMinimumLengthForEncode(size_t numBytes);
 
 		static
 			size_t ToBase64(const BYTE * const bytes, size_t numBytes, char encodedText[], size_t textLength);
-		template<class _Elem, class _Traits, class _Ax>
 		static
-			size_t ToBase64(const BYTE * const bytes, size_t numBytes, const std::basic_string<_Elem, _Traits, _Ax> & encodedText);
+			size_t ToBase64(const BYTE * const bytes, size_t numBytes, WCHAR encodedText[], size_t textLength);
+
 
 		template<class _Ax>
 		static 
@@ -59,10 +47,6 @@ namespace Earlgrey
 			encodedText.reserve(textLength);
 			return ToBase64String(bytes, numBytes, encodedText, textLength);
 		}
-		/*static
-			size_t ToBase64(const BYTE * const bytes, size_t numBytes, xstring& encodedText);*/
-		/*static
-			size_t ToBase64(const BYTE * const bytes, size_t numBytes, WCHAR encodedText[], size_t textLength);*/
 	};
 	
 
