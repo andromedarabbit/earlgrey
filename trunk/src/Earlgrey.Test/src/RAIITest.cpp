997d88e1dc42f4be29bf3a2bf9b2dafb7f1837c8
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-09-22 09:27:44 +0000

    handle_t 에 대한 단위테스트

--- trunk/src/Earlgrey.Test/src/RAIITest.cpp
+++ trunk/src/Earlgrey.Test/src/RAIITest.cpp
@@ -29,5 +29,41 @@ namespace Earlgrey
 			}
 			ASSERT_EQ(256, ArrayItem::DELETE_COUNT);			
 		}
+
+		namespace
+		{
+			
+			static bool HandleCalled = false;
+			BOOL CloseHandleT(HANDLE handle)
+			{				
+				HandleCalled = true;
+				return CloseHandle(handle);
+			}
+
+		}
+
+		TEST(RAIITest, HandleT)
+		{
+			{
+				ASSERT_FALSE(HandleCalled);
+
+				// create Wait event
+				HANDLE eventObj = CreateEvent(
+					(LPSECURITY_ATTRIBUTES) 0,
+					true,           // manual-reset
+					false,          // initial state off
+					(LPCTSTR) 0     // name
+					);
+				ASSERT_TRUE(eventObj != NULL);
+
+				Earlgrey::handle_t handle(eventObj, &CloseHandleT);
+				ASSERT_FALSE(HandleCalled);
+			}
+
+			ASSERT_TRUE(HandleCalled);
+
+			// Earlgrey::handle_t schServiceHandle(schService, &CloseServiceHandle);
+
+		}
 	}
 }
