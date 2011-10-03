ef4be73853be2b39a5dfcc70adea2a568b10d8ac
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-10-03 09:08:27 +0000

    openssl 정리

--- src/GmailClient/inc/base64.h
+++ src/GmailClient/inc/base64.h
@@ -1,9 +1,41 @@
+#pragma once
 #include <string>
 
-#ifndef _BASE64_H_
-#define _BASE64_H_
+#include "Convert.hpp"
 
-std::string base64_encode(unsigned char const* , size_t len);
-std::string base64_decode(std::string const& s);
+//std::string base64_encode(BYTE const * , size_t len);
+//std::string base64_decode(std::string const& s);
+//
+//
 
-#endif
+
+std::string base64_encode(BYTE const * bytes_to_encode, size_t in_len) 
+{
+	std::string base64Text;
+
+	const size_t len_b64_encoded = Earlgrey::Convert::ToBase64(
+		reinterpret_cast<const BYTE *>(bytes_to_encode)
+		, in_len + 1
+		, base64Text
+		);
+
+	EARLGREY_ASSERT(len_b64_encoded >= in_len);
+
+	return base64Text;
+}
+
+std::string base64_decode(std::string const& encoded_string) 
+{
+	size_t bytesNeeded = Earlgrey::Convert::GetMinimumBytesForDecode(encoded_string.length());
+	BYTE * bytes = new BYTE[bytesNeeded];
+
+	SIZE_T len = Earlgrey::Convert::FromBase64(encoded_string, bytes, bytesNeeded);
+	EARLGREY_ASSERT(len <= encoded_string.length());
+
+
+	std::string decodedText(reinterpret_cast<CHAR*>(bytes));
+	delete bytes;
+	return decodedText;
+}
+
+
