950191f09ae90572aa2329d800c88d37f4092403
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-10-20 06:24:43 +0000

    깨진 빌드 수정

--- branches/unitybuildsample/vendor/gurtle/register.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SETX /S "%COMPUTERNAME%" GURTLE_ISSUE_UPDATE_CMD "\"%~dp0scripts\updissue.cmd\" --username {username} --password {password} --project {project} --issue {issue.id} --status {status} --comment {comment} --debug"
-SET ERR_LEVEL=%errorlevel%
-if %ERR_LEVEL% NEQ 0 exit /b %ERR_LEVEL%
-
-exit /b 0
