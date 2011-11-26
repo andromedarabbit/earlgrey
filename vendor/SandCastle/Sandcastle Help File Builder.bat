2998fd5b5589d4ce682dda683478a55f2d349064
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-11-26 09:11:04 +0000

    C# 문서화 도구 Sandcastle 을 자동으로 설치하도록

--- vendor/SandCastle/Sandcastle Help File Builder.bat
+++ vendor/SandCastle/Sandcastle Help File Builder.bat
@@ -5,4 +5,6 @@ SET CURRENT_DIR=%~dp0
 SET SHFBROOT=%CURRENT_DIR%EWSoftware\Sandcastle Help File Builder
 SET DXROOT=%CURRENT_DIR%Sandcastle
 
+IF NOT EXIST "%DXROOT%" CALL "%CURRENT_DIR%..\..\src\Msbuild_Win32.bat" "%CURRENT_DIR%..\..\src\BuildTools\msbuild-initialize.xml" /t:Initialize
+
 "%SHFBROOT%\SandcastleBuilderGUI.exe" "%CURRENT_DIR%..\..\src\BuildTools\MSBuild\UserGuide\UserGuideProject.shfbproj"
