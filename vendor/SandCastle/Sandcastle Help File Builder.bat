aa12f5f86db75e9c57a9372f11aee034bae9ef48
plaintext@andromedarabbit.net <plaintext@andromedarabbit.net@bf4b8bca-1f5e-11de-a108-cd2f117ce590>
2011-11-29 15:27:56 +0000

    문서화 도구 - 압축 파일에 MAML 가이드와 HTML->MAML 변환 도구를 넣음

--- vendor/SandCastle/Sandcastle Help File Builder.bat
+++ vendor/SandCastle/Sandcastle Help File Builder.bat
@@ -3,7 +3,7 @@ SETLOCAL
 
 SET CURRENT_DIR=%~dp0
 SET SHFBROOT=%CURRENT_DIR%EWSoftware\Sandcastle Help File Builder
-SET DXROOT=%CURRENT_DIR%Sandcastle
+IF "%DXROOT%" EQU "" SET DXROOT=%CURRENT_DIR%Sandcastle
 
 IF NOT EXIST "%DXROOT%" CALL "%CURRENT_DIR%..\..\src\Msbuild_Win32.bat" "%CURRENT_DIR%..\..\src\BuildTools\msbuild-initialize.xml" /t:Initialize
 
